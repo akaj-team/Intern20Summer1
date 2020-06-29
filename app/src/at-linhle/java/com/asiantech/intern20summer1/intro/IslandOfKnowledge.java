0faea83b1f88b596a55fd53000c14081396219fc
at-linhle <linh.le@asiantech.vn>
2020-06-29 17:25:40 +0700

    ÃIslandOfknowledge

--- app/src/at-linhle/java/com/asiantech/intern20summer1/intro/IslandOfKnowledge.java
+++ app/src/at-linhle/java/com/asiantech/intern20summer1/intro/IslandOfKnowledge.java
@@ -32,7 +32,7 @@ public class IslandOfKnowledge {
     }
 
     /**
-     * An IP address is a numerical label assigned to each device (e.g., computer, printer)
+     * An IP address is a numerical label assigned to each device (e.g., computer)
      * participating in a computer network that uses the Internet Protocol for communication.
      * There are two versions of the Internet protocol, and thus two versions of addresses.
      * One of them is the IPv4 address.
