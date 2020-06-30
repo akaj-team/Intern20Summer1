383885cfac430aa6d93d9cfe818151a1e0c9ea42
huy.bui <huy.bui@asiantech.vn>
2020-06-30 17:55:29 +0700

    update to ThroughtheFog

--- app/src/at-huybui/java/com/asiantech/intern20summer1/intro/ThroughtheFog.java
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/intro/ThroughtheFog.java
@@ -1,4 +1,6 @@
-package com.asiantech.intern20summer1.intro;
+package com.asiantech.intern20mSummer1.intro;
+
+import java.util.Arrays;
 
 public class ThroughtheFog {
 
@@ -19,8 +21,8 @@ public class ThroughtheFog {
     /** 31
      * You have deposited a specific amount of money into your bank account.
      * Each year your balance increases at the same growth rate.
-     * With the assumption that you don't make any additional deposits,
-     * find out how long it would take for your balance to pass a specific threshold.
+     * With the asmSumption that you don't make any additional deposits,
+     * find mOut how long it would take for your balance to pass a specific threshold.
      */
 
         int depositProfit(int deposit, int rate, int threshold) {
@@ -33,4 +35,34 @@ public class ThroughtheFog {
             return mYear;
         }
 
+    /** 32
+     *Given a sorted array of integers a, your task is to determine which element of
+     *a is closest to all other values of a. In other words,
+     *find the element x in a, which minimizes the following mSum:
+     * abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+     * (where abs denotes the absolute value)
+     * If there are several possible answers, mOutput the smallest one.
+     */
+
+    
+    int absoluteValuesSumMinimization(int[] a) {
+
+        Arrays.sort(a); // nổi bọt giá mảng để được mảng tăng nghiêm ngặt
+        int mOut = a[0]; // giá trị phù hợp để xuất ra. giá trị này cần là a[0]
+        int mSum = 0;    // buffer tổng cho đa thức
+        for(int j:a){
+            mSum += Math.abs(a[0]-j); // gán giá trị đầu cho mSum
+        }
+        for(int i:a){
+            int mSumNew = 0;
+            for(int j:a){
+                mSumNew += Math.abs(i-j); //Tính tổng đa thức
+            }
+            if(mSumNew < mSum){ // Nếu mới nhỏ hơn tổng cũ thì ta gán lại các giá trị mSum và mOut
+                mSum = mSumNew;
+                mOut = i;
+            }
+        }
+        return mOut;
+    }
 }
