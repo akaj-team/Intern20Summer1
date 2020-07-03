29be292b0fc93681a25050e6378bfda495078ed1
Phuong Le T. D. [2] <phuong.le2@asiantech.vn>
2020-07-03 15:48:50 +0700

    update class EruptionOfLight

--- app/src/at-phuongle/java/com/asiantech/intern20summer1/intro/EruptionOfLight.java
+++ app/src/at-phuongle/java/com/asiantech/intern20summer1/intro/EruptionOfLight.java
@@ -4,7 +4,7 @@ public class EruptionOfLight {
     /**
      * Write a funtion that returns the domain part of the given email address.
      */
-    static String findEmailDomain(String address) {
+    String findEmailDomain(String address) {
         int indexOfAt = 0;
         for (int i = 0; i < address.length(); i++) {
             if (address.charAt(i) == '@') {
@@ -18,7 +18,30 @@ public class EruptionOfLight {
     /**
      * Write a funtion that checks if the given string corresponds to MAC-48 address naming rules.
      */
-    static boolean isMAC48Address(String inputString) {
+    boolean isMAC48Address(String inputString) {
         return inputString.matches("(([0-9A-F]{2}[:-]?){6})");
+    }
+
+    /**
+     * Write a function that returns the shortest possible string which
+     * can be achieved by adding characters to the end of initial string to make it a palindrome.
+     */
+    String buildPalindrome(String st) {
+        boolean canConvert;
+        for (int i = st.length(); ; i++) {
+            canConvert = true;
+            for (int j = 0; j < i - j - 1; j++) {
+                if (i - j <= st.length() && (st.charAt(j) != st.charAt(i - j - 1))) {
+                    canConvert = false;
+                    break;
+                }
+            }
+            if (canConvert) {
+                for (int j = st.length(); j < i; j++) {
+                    st += st.charAt(i - j - 1);
+                }
+                return st;
+            }
+        }
     }
 }
