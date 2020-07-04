ea503e599948a0a47b12e56174bb7c256e3888bd
at-longphan <long.phan@asiantech.vn>
2020-07-04 22:53:53 +0700

    upload 3 files: EdgeOfTheOcean.kt, SmoothSailing.kt, TheJourneyBegins.kt

---
+++ app/src/at-longphan/java/com/asiantech/intern20summer1/introKotlin/EdgeOfTheOcean.kt
@@ -0,0 +1,119 @@
+package com.asiantech.intern20summer1.introKotlin
+
+import java.util.*
+
+fun main() {
+    //RUN main() with Coverage
+    val obj: EdgeOfTheOcean = EdgeOfTheOcean()
+    println("Ex 4: " + obj.adjacentElementsProduct(intArrayOf(3, 6, -2, -5, 7, 3)))
+
+    println("Ex 5: " + obj.shapeArea(5))
+
+    println("Ex 6: " + obj.makeArrayConsecutive2(mutableListOf(6, 2, 3, 8)))
+
+    println("Ex 7: " + obj.almostIncreasingSequence(mutableListOf(1, 3, 2, 1)))
+
+    println(
+        "Ex 8: " + obj.matrixElementsSum(
+            mutableListOf(
+                mutableListOf(0, 1, 1, 2),
+                mutableListOf(0, 5, 0, 0),
+                mutableListOf(2, 0, 3, 3)
+            )
+        )
+    )
+}
+
+class EdgeOfTheOcean {
+    fun adjacentElementsProduct(inputArray: IntArray): Int {
+        /**
+         * Given an array of integers, find the pair of adjacent elements
+         * that has the largest product and return that product.
+         */
+        var max = inputArray[0] * inputArray[1]
+        for (i in 1 until inputArray.size - 1) {
+            if (inputArray[i] * inputArray[i + 1] > max) {
+                max = inputArray[i] * inputArray[i + 1]
+            }
+        }
+        return max
+    }
+
+    fun shapeArea(n: Int): Int {
+        /**
+         * Below we will define an n-interesting polygon.
+         * Your task is to find the area of a polygon for a given n.
+         *
+         * A 1-interesting polygon is just a square with a side of length 1.
+         * An n-interesting polygon is obtained by taking the n - 1-interesting polygon
+         * and appending 1-interesting polygons to its rim, side by side.
+         * You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+         */
+        return n * n + (n - 1) * (n - 1)
+    }
+
+    fun makeArrayConsecutive2(statues: MutableList<Int>): Int {
+        /**
+         * Ratiorg got statues of different sizes as a present from CodeMaster for his birthday,
+         * each statue having an non-negative integer size. Since he likes to make things perfect,
+         * he wants to arrange them from smallest to largest so that each statue
+         * will be bigger than the previous one exactly by 1. He may need some additional statues
+         * to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+         */
+        statues.sort()
+        return statues[statues.size - 1] - statues[0] - 1 - (statues.size - 2)
+    }
+
+    fun almostIncreasingSequence(a: MutableList<Int>): Boolean {
+        /**
+         * Given a sequence of integers as an array, determine whether it is possible to obtain
+         * a strictly increasing sequence by removing no more than one element from the array.
+
+        Note: sequence a0, a1, ..., an is considered to be a strictly increasing
+        if a0 < a1 < ... < an. Sequence containing only one element is also considered
+        to be strictly increasing.
+         */
+        if (a.size == 2) return true
+        var count: Int = 0
+        var i: Int = 0
+        while (i < a.size - 1) {
+            if (a[i] >= a[i + 1]) {
+                count++
+                if (count == 2) {
+                    return false
+                }
+                if (i > 0 && i < a.size - 2 && a[i + 2] <= a[i] && a[i + 1] <= a[i - 1]) {
+                    return false
+                }
+            }
+            i++
+        }
+        return true
+    }
+
+    fun matrixElementsSum(matrix: MutableList<MutableList<Int>>): Int {
+        /**
+         * After becoming famous, the CodeBots decided to move into a new building together.
+         * Each of the rooms has a different cost, and some of them are free,
+         * but there's a rumour that all the free rooms are haunted! Since the CodeBots are quite superstitious,
+         * they refuse to stay in any of the free rooms, or any of the rooms below any of the free rooms.
+
+        Given matrix, a rectangular matrix of integers, where each value represents the cost of the room,
+        your task is to return the total sum of all rooms that are suitable for the CodeBots
+        (ie: add up all the values that don't appear below a 0).
+         */
+        var row: Int = matrix.size
+        var col: Int = matrix[0].size
+        var sum: Int = 0
+        for (i in 0 until col) {
+            for (j in 0 until row) {
+                if (matrix[j][i] == 0) {
+                    break
+                }
+                sum += matrix[j][i]
+            }
+        }
+        return sum
+    }
+}
+
---
+++ app/src/at-longphan/java/com/asiantech/intern20summer1/introKotlin/SmoothSailing.kt
@@ -0,0 +1,124 @@
+package com.asiantech.intern20summer1.introKotlin
+
+fun main() {
+    //RUN main() with Coverage
+    var obj: SmoothSailing = SmoothSailing()
+    println("Ex 9: " + obj.allLongestStrings(mutableListOf("aba", "aa", "ad", "vcd", "aba")))
+
+    println("Ex 10: " + obj.commonCharacterCount("aabcc", "adcaa"))
+
+    println("Ex 11: " + obj.isLucky(1395))
+
+    println("Ex 12: " + obj.sortByHeight(mutableListOf(-1, 150, 190, 170, -1, -1, 160, 180)))
+
+    println("Ex 13: " + obj.reverseInParentheses("foo(bar)baz(blim)"))
+}
+
+class SmoothSailing {
+    fun allLongestStrings(inputArray: MutableList<String>): Array<String> {
+        /**
+         * Given an array of strings, return another array containing all of its longest strings.
+         */
+        var array: String = ""
+        var maxLength: Int = 0
+        for (str in inputArray) {
+            if (str.length > maxLength) {
+                maxLength = str.length
+                array = str + ","
+            } else if (str.length == maxLength) {
+                array += str + ","
+            }
+        }
+        return array.substring(0, array.length - 1).split(",").toTypedArray()
+    }
+
+    fun commonCharacterCount(s1: String, s2: String): Int {
+        /**
+         * Given two strings, find the number of common characters between them.
+         */
+        var s2copy = s2
+        var count: Int = 0
+        var s1Length = s1.length
+        for (i in 0 until s1Length) {
+            var ch = s1[i]
+            if (s2copy.indexOf(ch) != -1) {
+                count++
+                s2copy = charRemoveAt(s2copy, s2copy.indexOf(ch))
+            }
+        }
+        return count
+    }
+
+    fun charRemoveAt(str: String, charIndex: Int): String {
+        /**
+         * Remove a char at index 'charIndex' in string 'String'
+         */
+        return str.substring(0, charIndex) + str.substring(charIndex + 1)
+    }
+
+    fun isLucky(n: Int): Boolean {
+        /**
+         * Ticket numbers usually consist of an even number of digits.
+         * A ticket number is considered lucky if the sum of the first half
+         * of the digits is equal to the sum of the second half.
+
+        Given a ticket number n, determine if it's lucky or not.
+         */
+        var str: String = n.toString()
+        var result: Int = 0
+        var i: Int = 0
+        while (i < str.length / 2) {
+            result = result + str[i].toInt()
+            i++
+        }
+        while (i < str.length) {
+            result = result - str[i].toInt()
+            i++
+        }
+        return result == 0
+    }
+
+    fun sortByHeight(a: MutableList<Int>): MutableList<Int> {
+        /**
+         * Some people are standing in a row in a park. There are trees between them which cannot
+         * be moved. Your task is to rearrange the people by their heights in a non-descending
+         * order without moving the trees. People can be very tall!
+         */
+        var b = mutableListOf<Int>()
+        for (i in a) {
+            if (i != -1) {
+                b.add(i)
+            }
+        }
+        b.sort()
+        var listIndex: Int = 0
+        for ((index, item) in a.withIndex()) {
+            if (a[index] != -1) {
+                a[index] = b.get(listIndex)
+                listIndex++
+            }
+        }
+        return a
+    }
+
+    fun reverseInParentheses(a: String): String {
+        /**
+         * Write a function that reverses characters in (possibly nested)
+         * parentheses in the input string.
+
+        Input strings will always be well-formed with matching ()s.
+         */
+        var aClone = a
+        var firstIndex: Int = 0
+        var lastIndex: Int = 0
+        while (aClone.contains("(")) {
+            firstIndex = aClone.lastIndexOf("(")
+            lastIndex = aClone.indexOf(")", firstIndex)
+
+            var reverseStr = aClone.substring(firstIndex + 1, lastIndex).reversed()
+            aClone = aClone.substring(0, firstIndex) + reverseStr + aClone.substring(lastIndex + 1)
+        }
+        return aClone
+    }
+
+}
