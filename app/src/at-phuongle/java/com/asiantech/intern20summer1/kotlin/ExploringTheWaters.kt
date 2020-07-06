63aae19124fcafec432c01a8e9fe9ba08ceb2efb
Phuong Le T. D. [2] <phuong.le2@asiantech.vn>
2020-07-06 16:36:49 +0700

    reduce the number of return statement of function areSimilar()

--- app/src/at-phuongle/java/com/asiantech/intern20summer1/kotlin/ExploringTheWaters.kt
+++ app/src/at-phuongle/java/com/asiantech/intern20summer1/kotlin/ExploringTheWaters.kt
@@ -1,8 +1,8 @@
 package com.asiantech.intern20summer1.kotlin
 
 object ExploringTheWaters {
-    fun alternatingSums(a : IntArray) : IntArray {
-        var aSums : IntArray = IntArray(2)
+    fun alternatingSums(a: IntArray): IntArray {
+        var aSums: IntArray = IntArray(2)
 
         for (i in 0 until a.size) {
             if (i % 2 == 0) {
@@ -14,11 +14,11 @@ object ExploringTheWaters {
         return aSums;
     }
 
-    fun addBorder(picture : Array<String>) : Array<String?> {
-        var borderRow : Int = picture.size + 2
-        var borderCol : Int = picture[0].length + 2
-        var newPicture : Array<String?> = arrayOfNulls<String>(borderRow)
-        var tmp : String = ""
+    fun addBorder(picture: Array<String>): Array<String?> {
+        var borderRow: Int = picture.size + 2
+        var borderCol: Int = picture[0].length + 2
+        var newPicture: Array<String?> = arrayOfNulls<String>(borderRow)
+        var tmp: String = ""
 
         for (i in 0 until newPicture.size) {
             if (i == 0 || i == borderRow - 1) {
@@ -38,58 +38,62 @@ object ExploringTheWaters {
         return newPicture;
     }
 
-    class Position{
-        public var position : Int
-        public var aElement : Int
-        public var bElement : Int
+    class Position {
+        public var position: Int
+        public var aElement: Int
+        public var bElement: Int
 
-        constructor(pos : Int, a : Int, b : Int) {
+        constructor(pos: Int, a: Int, b: Int) {
             this.position = pos;
             this.aElement = a;
             this.bElement = b;
         }
     }
 
-    fun areSimilar(A : Array<Int>, B : Array<Int>) : Boolean {
-        var mismatchedPosition : MutableList<Position> = mutableListOf()
+    fun areSimilar(A: Array<Int>, B: Array<Int>): Boolean {
+        var check: Boolean;
+        var mismatchedPosition: MutableList<Position> = mutableListOf()
 
-        for(i in 0 until A.size){
-            var a : Int = A[i]
-            var b : Int = B[i]
+        for (i in 0 until A.size) {
+            var a: Int = A[i]
+            var b: Int = B[i]
 
-            if(a != b){
+            if (a != b) {
                 var p = Position(i, a, b);
                 mismatchedPosition.add(p);
             }
         }
 
-        if(mismatchedPosition.size == 0)
-            return true;
-        else if(mismatchedPosition.size == 2){
-            var pos1 : Position = mismatchedPosition.get(0);
-            var pos2 : Position = mismatchedPosition.get(1);
+        if (mismatchedPosition.size == 0) {
+            check = true;
+        } else if (mismatchedPosition.size == 2) {
+            var pos1: Position = mismatchedPosition.get(0);
+            var pos2: Position = mismatchedPosition.get(1);
 
-            var aPos1 : Int = pos1.aElement;
-            var bPos1 : Int = pos1.bElement;
+            var aPos1: Int = pos1.aElement;
+            var bPos1: Int = pos1.bElement;
 
-            var aPos2 : Int = pos2.aElement;
-            var bPos2 : Int = pos2.bElement;
+            var aPos2: Int = pos2.aElement;
+            var bPos2: Int = pos2.bElement;
 
-            if(aPos1 == bPos2 && bPos1 == aPos2)
-                return true;
-            else
-                return false;
+            if (aPos1 == bPos2 && bPos1 == aPos2) {
+                check = true;
+            } else {
+                check = false;
+            }
+        } else {
+            check = false;
         }
-        else
-            return false;
+
+        return check;
     }
 
-    fun arrayChange(inputArray : Array<Int>) : Int {
-        var result : Int = 0
+    fun arrayChange(inputArray: Array<Int>): Int {
+        var result: Int = 0
 
         for (i in 1 until inputArray.size) {
             if (inputArray[i - 1] >= inputArray[i]) {
-                var difference : Int = Math.abs(inputArray[i - 1] - inputArray[i]) + 1
+                var difference: Int = Math.abs(inputArray[i - 1] - inputArray[i]) + 1
                 inputArray[i] += difference
                 result += difference
             }
@@ -98,8 +102,8 @@ object ExploringTheWaters {
         return result
     }
 
-    fun palindromeRearranging(inputString : String) : Boolean {
-        var list : MutableList<Char> = mutableListOf()
+    fun palindromeRearranging(inputString: String): Boolean {
+        var list: MutableList<Char> = mutableListOf()
 
         for (i in 0 until inputString.length) {
             if (list.contains(inputString[i])) {
