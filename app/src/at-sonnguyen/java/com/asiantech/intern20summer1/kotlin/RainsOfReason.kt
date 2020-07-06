e111376e9ce3096242c7e154fa61fbdb5544a741
at-sonnguyen <son.nguyen@asiantech.vn>
2020-07-06 16:16:46 +0700

    add Class ThroughTheFrog

---
+++ app/src/at-sonnguyen/java/com/asiantech/intern20summer1/kotlin/RainsOfReason.kt
@@ -0,0 +1,71 @@
+package com.asiantech.intern20summer1.kotlin
+
+object rainsOfReason{
+    /*
+    *   Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+    */
+    private fun arrayReplace(inputArray: MutableList<Int>, elemToReplace: Int, substitutionElem: Int): MutableList<Int> {
+        for(i in inputArray.indices){
+            if (inputArray[i] == elemToReplace ){
+                inputArray[i] = substitutionElem
+            }
+        }
+
+        return inputArray
+    }
+    /*
+    * Check if all digits of the given integer are even.
+    */
+    private fun evenDigitsOnly(n: Int): Boolean {
+        var copyN : Int = n
+        while(copyN > 0){
+            if(copyN % 2 != 0 ){
+                return false
+            }
+            copyN/= 10
+        }
+        return true
+    }
+    /*
+    *   Check if the given string is a correct variable name.
+    */
+    private fun variableName(name: String): Boolean {
+        return """([a-zA-Z]|_)+(\w|_)*""".toRegex().matches(name)
+    }
+    /*
+    *   Given a string, your task is to replace each of its characters
+    *   by the next one in the English alphabet
+    */
+    private fun alphabeticShift(inputString: String): String {
+        var resultString : String = ""
+        for (i in inputString.indices){
+
+            if (inputString.get(i)+1 > 'z'){
+                resultString += "a"
+            }else resultString += (inputString.get(i)+1)
+        }
+        return resultString
+    }
+    /*
+    *   Given two cells on the standard chess board, determine whether they have the same color or not.
+    */
+    private fun chessBoardCellColor(cell1: String, cell2: String): Boolean {
+        if( ((cell1.get(0).toInt()+cell1.get(1).toInt())  %2 == 0 &&(cell2.get(0).toInt()+ cell2.get(1).toInt())%2 == 0 || (cell1.get(0).toInt()+cell1.get(1).toInt()) %2 !=0 && (cell2.get(0).toInt()+ cell2.get(1).toInt())%2 != 0)){
+            return true
+        }
+        return false
+    }
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val inputArray : MutableList<Int> = mutableListOf(1, 2, 1)
+        val n : Int = 268462
+        val string = "variable0"
+        val cell1 = "A1"
+        val cell2 = "H2"
+        println("25. Replace all element = 1 to element = 3 in $inputArray is ${arrayReplace(inputArray,1,3)} ")
+        println("26. $n is only even Digits ? ${evenDigitsOnly(n)} ")
+        println("27. $string is a variable name ? ${variableName(string)} ")
+        println("28. $string after shift a value is ${alphabeticShift(string)}")
+        println("29. $cell1 and $cell2 are same color ? ${chessBoardCellColor(cell1,cell2)} ")
+    }
+}
---
+++ app/src/at-sonnguyen/java/com/asiantech/intern20summer1/kotlin/ThroughTheFog.kt
@@ -0,0 +1,80 @@
+package com.asiantech.intern20summer1.kotlin
+
+object throughTheFog{
+    /*
+    *   Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+    */
+    private fun circleOfNumbers(n: Int, firstNumber: Int): Int = (firstNumber+n/2)%n
+    /*
+    *   how long it would take for your balance to pass a specific threshold
+    */
+    private fun depositProfit(deposit: Int, rate: Int, threshold: Int): Int {
+        var result : Int = 0
+        var money : Double = deposit + 0.0
+        while (money < threshold){
+            result++
+            money += money*rate/100
+        }
+        return result
+    }
+    /*
+    *   Given a sorted array of integers a,
+    *   task is to determine which element of a is closest to all other values of a.
+    *   In other words, find the element x in a, which minimizes the following sum:
+    */
+    private fun absoluteValuesSumMinimization(a: MutableList<Int>): Int = a[(a.size-1)/2]
+    /*
+    *   Given an array of equal-length strings, you'd like to know
+    * if it's possible to rearrange the order of the elements
+    * in such a way that each consecutive pair of strings differ by exactly one character.
+    * Return true if it's possible, and false if not.
+    */
+    private open fun stringsRearrangement(inputArray:  MutableList<String>): Boolean {
+        return p(inputArray.size, inputArray.size, inputArray, false)
+    }
+
+    fun p(n: Int, s: Int, a:  MutableList<String>, b: Boolean): Boolean {
+        var b = b
+        if (s == 1) {
+            var c = 0
+            for (i in 0 until n - 1) {
+                for (j in a[i].indices) {
+                    if (a[i].substring(j, j + 1) != a[i + 1].substring(j, j + 1)) {
+                        c++
+                    }
+                }
+                if (a[i] == a[i + 1]) {
+                    c += 10
+                }
+            }
+            if (c == a.size - 1) {
+                return true
+            }
+        }
+        for (i in 0 until s) {
+            b = p(n, s - 1, a, b)
+            if (s % 2 == 0) {
+                val t = a[i]
+                a[i] = a[s - 1]
+                a[s - 1] = t
+            } else {
+                val t = a[0]
+                a[0] = a[s - 1]
+                a[s - 1] = t
+            }
+        }
+        return b
+    }
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val deposit = 100
+        val rate = 20
+        val threshold = 200
+        val intArray : MutableList<Int> = mutableListOf(-10, 100, 200, 300, 400, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500)
+        val inputStringArray : MutableList<String> = mutableListOf("ab","bb","aa")
+        println("30.result: ${circleOfNumbers(15,10)}")
+        println("31. spend ${depositProfit(100,20,200)} month to gain $threshold from $deposit with rate = $rate")
+        println("32. min of absolute value Sum in $intArray is ${absoluteValuesSumMinimization(intArray)}")
+        println("33. Result : ${stringsRearrangement(inputStringArray)}")
+    }
+}
