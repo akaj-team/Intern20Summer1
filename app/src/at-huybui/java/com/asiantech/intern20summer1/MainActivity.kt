51ae0d648af8ba6e4d94b3bfc3dcf2e72a88614f
huy.bui <huy.bui@asiantech.vn>
2020-07-07 14:24:18 +0700

    update debug java

--- app/src/at-huybui/java/com/asiantech/intern20summer1/MainActivity.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/MainActivity.kt
@@ -2,16 +2,10 @@ package com.asiantech.intern20summer1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.asiantech.intern20summer1.introJava.DarkWilderness
-import com.asiantech.intern20summer1.introKotlin.TheJourneyBegins
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var huy : TheJourneyBegins = TheJourneyBegins();
-
-        huy.add(10,20);
     }
 }
