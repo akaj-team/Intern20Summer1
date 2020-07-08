e30446d190ce1c359096525e0d937b89bdff5d9d
Phuong Le T. D. [2] <phuong.le2@asiantech.vn>
2020-07-08 15:49:35 +0700

    fix magic numbers

--- app/src/at-phuongle/java/com/asiantech/intern20summer1/kotlin/ThroughTheFog.kt
+++ app/src/at-phuongle/java/com/asiantech/intern20summer1/kotlin/ThroughTheFog.kt
@@ -1,23 +1,25 @@
 package com.asiantech.intern20summer1.kotlin
 
 object ThroughTheFog {
+    private const val PERCENT = 100.0
+
     fun circleOfNumbers(n: Int, firstNumber: Int): Int {
         var numbers1: IntArray = IntArray(n / 2)
         var numbers2: IntArray = IntArray(numbers1.size)
         var m: Int = 0
         var indexOfOpposite: Int = 0
 
-        for (i in 0 until numbers1.size) {
+        for (i in numbers1.indices) {
             numbers1[i] = m
             m++
         }
 
-        for (i in 0 until numbers2.size) {
+        for (i in numbers2.indices) {
             numbers2[i] = m
             m++
         }
 
-        for (i in 0 until numbers1.size) {
+        for (i in numbers1.indices) {
             if (firstNumber == numbers1[i]) {
                 indexOfOpposite = i
                 return numbers2[indexOfOpposite]
@@ -30,7 +32,7 @@ object ThroughTheFog {
     }
 
     fun depositProfit(deposit: Int, rate: Int, threshold: Int): Int {
-        var r: Double = rate / 100.0
+        var r: Double = rate / PERCENT
         var myDeposit: Double = deposit.toDouble()
         var numberOfYear: Int = 0
         while (true) {
