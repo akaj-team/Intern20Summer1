2cd9e78a16cd7ce2a48913d59bf6bf361c911ea2
Phuong Le T. D. [2] <phuong.le2@asiantech.vn>
2020-07-08 17:39:48 +0700

    update object EruptionOfLight

--- app/src/at-phuongle/java/com/asiantech/intern20summer1/kotlin/EruptionOfLight.kt
+++ app/src/at-phuongle/java/com/asiantech/intern20summer1/kotlin/EruptionOfLight.kt
@@ -16,4 +16,32 @@ object EruptionOfLight {
         var regex = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
         return inputString.matches(regex.toRegex())
     }
+
+    fun buildPalindrome(st: String): String {
+        var string: String = st
+        var canConvert: Boolean
+        var i: Int = string.length
+        var j: Int = 0
+
+        while (true) {
+            canConvert = true
+
+            for (k in 0 until (i - j - 1)) {
+                if (i - k <= string.length && (string[k] != string[i - k - 1])) {
+                    canConvert = false
+                    break
+                }
+            }
+
+            if (canConvert) {
+                for (j in string.length until i) {
+                    string += string[i - j - 1]
+                }
+                return string
+            }
+
+            i++
+            j++
+        }
+    }
 }
