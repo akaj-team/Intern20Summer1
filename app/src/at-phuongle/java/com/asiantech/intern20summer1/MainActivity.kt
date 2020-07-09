974805bdc24a7156225ed65d8bf03dd8d3041e25
Phuong Le T. D. [2] <phuong.le2@asiantech.vn>
2020-07-09 20:58:08 +0700

    handle event button sign up and text view sign up

--- app/src/at-phuongle/java/com/asiantech/intern20summer1/MainActivity.kt
+++ app/src/at-phuongle/java/com/asiantech/intern20summer1/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.asiantech.intern20summer1
 
+import android.R
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.asiantech.intern20summer1.R.layout.activity_main)
+    }
+
+    fun onSignUp(v: View?) {
+        Toast.makeText(applicationContext,"Sign up!",Toast.LENGTH_SHORT).show()
+    }
+
+    fun onSignUpBtn(view: View?) {
+        Toast.makeText(applicationContext,"Sign Up",Toast.LENGTH_SHORT).show()
     }
 }
