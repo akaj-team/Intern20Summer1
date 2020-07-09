6ecb96d08ff846876dc368785dc8cbe24b21c665
huy.bui <huy.bui@asiantech.vn>
2020-07-09 23:01:10 +0700

    commit end day 23:00 2020/07/09

--- app/src/at-huybui/java/com/asiantech/intern20summer1/MainActivity.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.asiantech.intern20summer1
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.`at-huybui`.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handleForEdittextEmail()
+
+
+        btnSignup.setOnClickListener {
+            Log.d("AAA","Click buton")
+        }
+
+
+    }
+
+    private fun handleForEdittextEmail(){
+        edtEmail.setOnFocusChangeListener { view, b ->
+            if(b == true){
+                printLog("chọn email Edittext")
+
+            }else{
+                printLog("bỏ chọn email Edittext")
+            }
+        }
+
+        edtEmail.addTextChangedListener{
+            printLog("change text")
+        }
+
+    }
+
+    private fun printLog(st :String){
+        Log.d("AAA",st)
     }
 }
