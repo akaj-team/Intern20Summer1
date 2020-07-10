11e2f5662ea924b98728713138a922093a8593fa
Phuong Le T. D. [2] <phuong.le2@asiantech.vn>
2020-07-10 15:18:54 +0700

    fix warnings

--- app/src/at-phuongle/java/com/asiantech/intern20summer1/MainActivity.kt
+++ app/src/at-phuongle/java/com/asiantech/intern20summer1/MainActivity.kt
@@ -16,119 +16,109 @@ private const val PASSWORD_LENGTH = 6
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.asiantech.intern20summer1.R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        val fieldEmail = findViewById<EditText>(com.asiantech.intern20summer1.R.id.edtEmail)
-        val imgEmail = findViewById<ImageView>(com.asiantech.intern20summer1.R.id.imgEmail)
-        val fieldPass = findViewById<EditText>(com.asiantech.intern20summer1.R.id.edtPass)
-        val imgPass = findViewById<ImageView>(com.asiantech.intern20summer1.R.id.imgPass)
-        val fieldPass2 = findViewById<EditText>(com.asiantech.intern20summer1.R.id.edtPass2)
-        val imgPass2 = findViewById<ImageView>(com.asiantech.intern20summer1.R.id.imgPass2)
+        val fieldEmail = findViewById<EditText>(R.id.edtEmail)
+        val imgEmail = findViewById<ImageView>(R.id.imgEmail)
+        val fieldPass = findViewById<EditText>(R.id.edtPass)
+        val imgPass = findViewById<ImageView>(R.id.imgPass)
+        val fieldPass2 = findViewById<EditText>(R.id.edtPass2)
+        val imgPass2 = findViewById<ImageView>(R.id.imgPass2)
 
-        fieldEmail.onFocusChangeListener = object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                if (hasFocus && fieldEmail.text.toString().isNullOrEmpty()) {
-                    fieldEmail.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border_active)
-                    imgEmail.setBackgroundResource(0)
-                } else if (!hasFocus && fieldEmail.text.toString().isNullOrEmpty()) {
-                    fieldEmail.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border_normal)
-                    imgEmail.setBackgroundResource(0)
+        fieldEmail.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus && fieldEmail.text.toString().isEmpty()) {
+                fieldEmail.setBackgroundResource(R.drawable.edit_text_border_active)
+                imgEmail.setBackgroundResource(0)
+            } else if (!hasFocus && fieldEmail.text.toString().isEmpty()) {
+                fieldEmail.setBackgroundResource(R.drawable.edit_text_border_normal)
+                imgEmail.setBackgroundResource(0)
+            } else {
+                if (isEmailValid(fieldEmail.text.toString())) {
+                    fieldEmail.setBackgroundResource(R.drawable.edit_text_border)
+                    imgEmail.setBackgroundResource(R.drawable.icon_tick)
+                    imgEmail.layoutParams.width = ICON_WIDTH
+                    imgEmail.requestLayout()
                 } else {
-                    if (isEmailValid(fieldEmail.text.toString())) {
-                        fieldEmail.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border)
-                        imgEmail.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.icon_tick)
-                        imgEmail.layoutParams.width = ICON_WIDTH
-                        imgEmail.requestLayout()
-                    } else {
-                        fieldEmail.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border_wrong)
-                        imgEmail.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.icon_error)
-                        imgEmail.layoutParams.width = ICON_WIDTH
-                        imgEmail.requestLayout()
-                    }
+                    fieldEmail.setBackgroundResource(R.drawable.edit_text_border_wrong)
+                    imgEmail.setBackgroundResource(R.drawable.icon_error)
+                    imgEmail.layoutParams.width = ICON_WIDTH
+                    imgEmail.requestLayout()
                 }
             }
         }
 
-        fieldPass.onFocusChangeListener = object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                if (hasFocus && fieldPass.text.toString().isNullOrEmpty()) {
-                    fieldPass.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border_active)
-                    imgPass.setBackgroundResource(0)
-                } else if (!hasFocus && fieldPass.text.toString().isNullOrEmpty()) {
-                    fieldPass.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border_normal)
-                    imgPass.setBackgroundResource(0)
+        fieldPass.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus && fieldPass.text.toString().isEmpty()) {
+                fieldPass.setBackgroundResource(R.drawable.edit_text_border_active)
+                imgPass.setBackgroundResource(0)
+            } else if (!hasFocus && fieldPass.text.toString().isEmpty()) {
+                fieldPass.setBackgroundResource(R.drawable.edit_text_border_normal)
+                imgPass.setBackgroundResource(0)
+            } else {
+                if (isPasswordValid(fieldPass.text.toString())) {
+                    fieldPass.setBackgroundResource(R.drawable.edit_text_border)
+                    imgPass.setBackgroundResource(R.drawable.icon_tick)
+                    imgPass.layoutParams.width = ICON_WIDTH
+                    imgPass.requestLayout()
                 } else {
-                    if (isPasswordValid(fieldPass.text.toString())) {
-                        fieldPass.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border)
-                        imgPass.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.icon_tick)
-                        imgPass.layoutParams.width = ICON_WIDTH
-                        imgPass.requestLayout()
-                    } else {
-                        fieldPass.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border_wrong)
-                        imgPass.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.icon_error)
-                        imgPass.layoutParams.width = ICON_WIDTH
-                        imgPass.requestLayout()
-                    }
+                    fieldPass.setBackgroundResource(R.drawable.edit_text_border_wrong)
+                    imgPass.setBackgroundResource(R.drawable.icon_error)
+                    imgPass.layoutParams.width = ICON_WIDTH
+                    imgPass.requestLayout()
                 }
             }
         }
 
-        fieldPass2.onFocusChangeListener = object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                if (hasFocus && fieldPass2.text.toString().isNullOrEmpty()) {
-                    fieldPass2.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border_active)
-                    imgPass2.setBackgroundResource(0)
-                } else if (!hasFocus && fieldPass2.text.toString().isNullOrEmpty()) {
-                    fieldPass2.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border_normal)
-                    imgPass2.setBackgroundResource(0)
+        fieldPass2.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus && fieldPass2.text.toString().isEmpty()) {
+                fieldPass2.setBackgroundResource(R.drawable.edit_text_border_active)
+                imgPass2.setBackgroundResource(0)
+            } else if (!hasFocus && fieldPass2.text.toString().isEmpty()) {
+                fieldPass2.setBackgroundResource(R.drawable.edit_text_border_normal)
+                imgPass2.setBackgroundResource(0)
+            } else {
+                if (isPassword2Valid(fieldPass.text.toString(), fieldPass2.text.toString())) {
+                    fieldPass2.setBackgroundResource(R.drawable.edit_text_border)
+                    imgPass2.setBackgroundResource(R.drawable.icon_tick)
+                    imgPass2.layoutParams.width = ICON_WIDTH
+                    imgPass2.requestLayout()
                 } else {
-                    if (isPassword2Valid(fieldPass.text.toString(), fieldPass2.text.toString())) {
-                        fieldPass2.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border)
-                        imgPass2.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.icon_tick)
-                        imgPass2.layoutParams.width = ICON_WIDTH
-                        imgPass2.requestLayout()
-                    } else {
-                        fieldPass2.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.edit_text_border_wrong)
-                        imgPass2.setBackgroundResource(com.asiantech.intern20summer1.R.drawable.icon_error)
-                        imgPass2.layoutParams.width = ICON_WIDTH
-                        imgPass2.requestLayout()
-                    }
+                    fieldPass2.setBackgroundResource(R.drawable.edit_text_border_wrong)
+                    imgPass2.setBackgroundResource(R.drawable.icon_error)
+                    imgPass2.layoutParams.width = ICON_WIDTH
+                    imgPass2.requestLayout()
                 }
             }
         }
     }
 
-    fun onSignUp(v: View?) {
+    fun onSignUp() {
         Toast.makeText(applicationContext, "Sign up!", Toast.LENGTH_SHORT).show()
     }
 
-    fun onSignUpBtn(view: View?) {
+    fun onSignUpBtn() {
         Toast.makeText(applicationContext, "Sign Up", Toast.LENGTH_SHORT).show()
     }
 
-    fun isEmailValid(email: String?): Boolean {
-        val expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"
+    private fun isEmailValid(email: String?): Boolean {
+        val expression = "^[\\w.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"
         val pattern: Pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
-        val matcher: Matcher = pattern.matcher(email)
+        val matcher: Matcher = pattern.matcher(email.toString())
 
         return matcher.matches()
     }
 
-    fun isPasswordValid(pass: String): Boolean {
-        var check: Boolean = true
+    private fun isPasswordValid(pass: String): Boolean {
+        var check = true
 
-        if (pass.length < PASSWORD_LENGTH) {
-            check = false
-        }
+        if (pass.length < PASSWORD_LENGTH) check = false
 
-        if (!pass[0].isUpperCase()) {
-            check = false
-        }
+        if (!pass[0].isUpperCase()) check = false
 
         return check
     }
 
-    fun isPassword2Valid(pass: String, pass2: String): Boolean {
+    private fun isPassword2Valid(pass: String, pass2: String): Boolean {
         return pass2 == pass
     }
 }
