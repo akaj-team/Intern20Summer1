f73cd0f2500fa38b90a26d2971c75141e92f8f85
huy.bui <huy.bui@asiantech.vn>
2020-07-13 12:00:36 +0700

    update debug

---
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/Any.kt
@@ -0,0 +1,14 @@
+package values
+
+import android.content.Context
+import android.widget.Toast
+
+
+/**
+ * This function will handle toast, it will used to show string to activity display
+ */
+private var toastStatus: Toast? = null
+fun Any.toast(context: Context, duration: Int = Toast.LENGTH_SHORT) {
+    toastStatus?.cancel()
+    toastStatus = Toast.makeText(context, this.toString(), duration).apply { show() }
+}
