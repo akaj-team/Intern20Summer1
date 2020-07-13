be005817cf5ebf0c9a12b08b3f41319ee4221a78
at-longphan <long.phan@asiantech.vn>
2020-07-13 15:27:54 +0700

    Resolve comments in review

--- app/src/at-longphan/java/com/asiantech/intern20summer1/MainActivity.kt
+++ app/src/at-longphan/java/com/asiantech/intern20summer1/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.asiantech.intern20summer1
 
 import android.content.Context
-import android.media.session.MediaSession
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
@@ -14,11 +15,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
-        handleEdtEmailId()
-        handleEdtPassword()
-        handleEdtRetypePassword()
+        handleEditTextEmailIdEvent()
+        handleEditTextPasswordEvent()
+        handleEditTextRetypePasswordEvent()
         handleListener()
     }
 
@@ -28,75 +29,110 @@ class MainActivity : AppCompatActivity() {
             view.hideKeyboard()
             true
         }
+
         btnSignup.setOnClickListener {
             Toast.makeText(applicationContext, "Sign Up", Toast.LENGTH_SHORT).show()
         }
+
         tvSignup.setOnClickListener {
             Toast.makeText(applicationContext, "Sign Up!", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun View.hideKeyboard() {
-        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
-    }
-
-    private fun handleEdtEmailId() {
+    private fun handleEditTextEmailIdEvent() {
         edtEmailId.addTextChangedListener {
             val emailId = edtEmailId.text.toString()
             if (emailId.isNotEmpty()) {
-                imgCheckEmailId.visibility = View.VISIBLE
-                if (isValidEmailId(emailId)) {
-                    imgCheckEmailId.setImageResource(R.drawable.icon_tick)
-                    edtEmailId.setBackgroundResource(R.drawable.custom_edt_valid)
-                } else {
-                    imgCheckEmailId.setImageResource(R.drawable.icon_error)
-                    edtEmailId.setBackgroundResource(R.drawable.custom_edt_invalid)
-                }
+                handleValid(
+                    isValidEmailId(emailId),
+                    imgCheckEmailId,
+                    edtEmailId
+                )
+            }
+        }
+
+        edtEmailId.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.setBackgroundResource(R.drawable.custom_edt_focused)
+            } else {
+                handleValid(
+                    isValidEmailId(edtEmailId.text.toString()),
+                    imgCheckEmailId,
+                    edtEmailId
+                )
             }
         }
     }
 
-    private fun handleEdtPassword() {
+    private fun handleEditTextPasswordEvent() {
         edtPassword.addTextChangedListener {
             val password = edtPassword.text.toString()
             if (password.isNotEmpty()) {
-                imgCheckPassword.visibility = View.VISIBLE
-                if (isValidPassword(password)) {
-                    imgCheckPassword.setImageResource(R.drawable.icon_tick)
-                    edtPassword.setBackgroundResource(R.drawable.custom_edt_valid)
-                } else {
-                    imgCheckPassword.setImageResource(R.drawable.icon_error)
-                    edtPassword.setBackgroundResource(R.drawable.custom_edt_invalid)
-                }
+                handleValid(
+                    isValidPassword(password),
+                    imgCheckPassword,
+                    edtPassword
+                )
+            }
+        }
+
+        edtPassword.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.setBackgroundResource(R.drawable.custom_edt_focused)
+            } else {
+                handleValid(
+                    isValidPassword(edtPassword.text.toString()),
+                    imgCheckPassword,
+                    edtPassword
+                )
             }
         }
     }
 
-    private fun handleEdtRetypePassword() {
+    private fun handleEditTextRetypePasswordEvent() {
         edtRetypePassword.addTextChangedListener {
             val password = edtPassword.text.toString()
             val retypePassword = edtRetypePassword.text.toString()
             if (retypePassword.isNotEmpty()) {
-                imgCheckRetypePassword.visibility = View.VISIBLE
-                if (password.equals(retypePassword)) {
-                    imgCheckRetypePassword.setImageResource(R.drawable.icon_tick)
-                    edtRetypePassword.setBackgroundResource(R.drawable.custom_edt_valid)
-                } else {
-                    imgCheckRetypePassword.setImageResource(R.drawable.icon_error)
-                    edtRetypePassword.setBackgroundResource(R.drawable.custom_edt_invalid)
-                }
+                handleValid(
+                    password == retypePassword,
+                    imgCheckRetypePassword,
+                    edtRetypePassword
+                )
+            }
+        }
+
+        edtRetypePassword.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                view.setBackgroundResource(R.drawable.custom_edt_focused)
+            } else {
+                handleValid(
+                    edtPassword.text.toString() == edtRetypePassword.text.toString(),
+                    imgCheckRetypePassword,
+                    edtRetypePassword
+                )
             }
         }
     }
 
+    private fun handleValid(expression: Boolean, imageView: ImageView, editText: EditText) {
+        imageView.visibility = View.VISIBLE
+        if (expression) {
+            imageView.setImageResource(R.drawable.icon_tick)
+            editText.setBackgroundResource(R.drawable.custom_edt_valid)
+        } else {
+            imageView.setImageResource(R.drawable.icon_error)
+            editText.setBackgroundResource(R.drawable.custom_edt_invalid)
+        }
+    }
+
     private fun isValidEmailId(email: String): Boolean {
-        val regexForEmail = "^[a-z][a-z0-9_.]{5,31}[@][a-z0-9]{2,}(.[a-z0-9]{2,4}){1,2}${'$'}".toRegex()
+        val regexForEmail =
+            "^[a-z][a-z0-9_.]{5,32}[@][a-z0-9]{2,}([.][a-z0-9]{2,4}){1,2}${'$'}".toRegex()
         return email.matches(regexForEmail)
     }
 
     private fun isValidPassword(password: String): Boolean {
-        val regexForPassword = "^[A-Z]{1}[a-zA-Z0-9_.]{5,31}${'$'}".toRegex()
-        return password.matches(regexForPassword)
+        return if (password.length >= 6 && password[0].isUpperCase()) true else false
     }
 }
