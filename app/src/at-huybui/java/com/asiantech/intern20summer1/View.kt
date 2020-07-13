9713e19c386ba04b7f631d0d9d3754e30dffcbe2
huy.bui <huy.bui@asiantech.vn>
2020-07-13 13:53:33 +0700

    update debug

--- app/src/at-huybui/java/com/asiantech/intern20summer1/View.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/View.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
+/**
+ * Function used to hide Keyboard
+ */
 fun View.hideKeyboard() {
     val inputMethodManager =
         context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
