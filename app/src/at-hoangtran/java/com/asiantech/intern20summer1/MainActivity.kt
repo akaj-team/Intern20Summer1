a8d805629ffbf7fe99185d9553a61dd0b3b548de
Hoang <hoang.tran3@asiantech.vn>
2020-07-14 17:00:15 +0700

    fix

--- app/src/at-hoangtran/java/com/asiantech/intern20summer1/MainActivity.kt
+++ app/src/at-hoangtran/java/com/asiantech/intern20summer1/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.asiantech.intern20summer1
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.RelativeLayout
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.`at-hoangtran`.activity_main.*
+import kotlinx.android.synthetic.`at-hoangtran`.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("ClickableViewAccessibility")
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    @SuppressLint("ClickableViewAccessibility", "ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,9 +27,10 @@ class MainActivity : AppCompatActivity() {
         edtPass.onFocusEditText(rlPass)
         edtRetype.onFocusEditText(rlRetype)
 
+
         lnMain?.setOnTouchListener { it, _ ->
-            it.requestFocus()
             it.clearFocus()
+            it.requestFocus()
             this.hideSoftKeyboard()
             true
         }
@@ -28,20 +39,11 @@ class MainActivity : AppCompatActivity() {
         edtPass.onTextChange(edtPass, imgPassTick, rlPass)
         edtRetype.onTextChange(edtRetype, imgRetypeTick, rlRetype)
 
-        tvSignUp?.setOnClickListener {
-            "Sign up".toast(this@MainActivity)
-        }
-        imgFB?.setOnClickListener {
-            "Facebook".toast(this@MainActivity)
-        }
-        imgTwitter?.setOnClickListener {
-            "Twitter".toast(this@MainActivity)
-        }
-        imgGGPlus?.setOnClickListener {
-            "Google+".toast(this@MainActivity)
-        }
-        btnSignUp?.setOnClickListener {
-            "Sign up".toast(this@MainActivity)
+        toastClick()
+        if(Build.VERSION.SDK_INT>=23){
+            window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            window.statusBarColor = R.color.White
         }
     }
 
@@ -53,5 +55,78 @@ class MainActivity : AppCompatActivity() {
                 rl.setBackgroundResource(R.drawable.bg_edit_text)
             }
         }
+    }
+
+     private fun toastClick(){
+         tvSignUp?.setOnClickListener {
+             "Sign up".toast(this@MainActivity)
+         }
+         imgFB?.setOnClickListener {
+             "Facebook".toast(this@MainActivity)
+         }
+         imgTwitter?.setOnClickListener {
+             "Twitter".toast(this@MainActivity)
+         }
+         imgGGPlus?.setOnClickListener {
+             "Google+".toast(this@MainActivity)
+         }
+         btnSignUp?.setOnClickListener {
+             "Sign up".toast(this@MainActivity)
+         }
+     }
+
+    fun validPassword(password: String): Boolean {
+        val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
+        return password.matches(passwordPattern.toRegex())
+    }
+
+    private fun EditText.onTextChange(edt: EditText, tick: ImageView, rl: RelativeLayout) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val str = edt.text.toString()
+                if (str.isEmpty()) {
+                    tick.visibility = View.INVISIBLE
+                    rl.setBackgroundResource(R.drawable.bg_edit_text_focus)
+                } else when (edt) {
+                    edtEmail -> {
+                        if (Patterns.EMAIL_ADDRESS.matcher(str).matches()) {
+                            tick.visibility = View.VISIBLE
+                            tick.setImageResource(R.mipmap.icon_tick)
+                        } else {
+                            tick.visibility = View.VISIBLE
+                            tick.setImageResource(R.mipmap.icon_error)
+                        }
+                    }
+                    edtPass -> {
+                        if (validPassword(str)) {
+                            tick.visibility = View.VISIBLE
+                            tick.setImageResource(R.mipmap.icon_tick)
+                            rl.setBackgroundResource(R.drawable.bg_edit_text_focus)
+                        } else {
+                            tick.visibility = View.VISIBLE
+                            tick.setImageResource(R.mipmap.icon_error)
+                            rl.setBackgroundResource(R.drawable.bg_wrong_password)
+                        }
+                    }
+                    edtRetype -> {
+                        if (str == edtPass.text.toString() && validPassword(str)) {
+                            tick.visibility = View.VISIBLE
+                            tick.setImageResource(R.mipmap.icon_tick)
+                            rl.setBackgroundResource(R.drawable.bg_edit_text_focus)
+                        } else {
+                            tick.visibility = View.VISIBLE
+                            tick.setImageResource(R.mipmap.icon_error)
+                            rl.setBackgroundResource(R.drawable.bg_wrong_password)
+                        }
+                    }
+                }
+            }
+        })
     }
 }
