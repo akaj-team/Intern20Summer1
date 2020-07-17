7a508b6f1d2a726adaf8d63bf8525b39ec6212ee
huy.bui <huy.bui@asiantech.vn>
2020-07-17 14:33:57 +0700

    commit

---
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.asiantech.intern20summer1
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.`at-huybui`.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val timer = object : CountDownTimer(5000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                tvSplash.text = "Splast  ${millisUntilFinished / 1000}"
+            }
+
+            override fun onFinish() {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        timer.start()
+    }
+
+    override fun onStop() {
+        finish()
+        super.onStop()
+    }
+}
---
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/fragment/SignInFragment.kt
@@ -0,0 +1,19 @@
+package com.asiantech.intern20summer1.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.asiantech.intern20summer1.R
+
+class SignInFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_in,container,false)
+    }
+
+}
