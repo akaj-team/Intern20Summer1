20a6895127b571e4d405e2668fc7655703aa2171
huy.bui <huy.bui@asiantech.vn>
2020-07-17 14:45:53 +0700

    commit 2

--- app/src/at-huybui/java/com/asiantech/intern20summer1/MainActivity.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/MainActivity.kt
@@ -2,12 +2,17 @@ package com.asiantech.intern20summer1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.asiantech.intern20summer1.fragment.SignInFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = SignInFragment()
+        fragmentTransaction.add(R.id.llMain, fragment)
+        fragmentTransaction.commit()
     }
 }
