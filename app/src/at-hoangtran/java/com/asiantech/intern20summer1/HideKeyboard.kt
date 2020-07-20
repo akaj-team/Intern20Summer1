f2682013d988393a573a4b3ccc06c01c03a84a70
Hoang <hoang.tran3@asiantech.vn>
2020-07-20 15:29:23 +0700

    fix

--- app/src/at-hoangtran/java/com/asiantech/intern20summer1/HideKeyboard.kt
+++ app/src/at-hoangtran/java/com/asiantech/intern20summer1/HideKeyboard.kt
@@ -8,7 +8,7 @@ import androidx.core.content.ContextCompat
 
 fun Activity.hideSoftKeyboard() {
     currentFocus?.let {
-        val inputMethodManager = ContextCompat.getSystemService(this, InputMethodManager::class.java)!!
-        inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+        val inputMethodManager = ContextCompat.getSystemService(this, InputMethodManager::class.java)
+        inputMethodManager?.hideSoftInputFromWindow(it.windowToken, 0)
     }
 }
