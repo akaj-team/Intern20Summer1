ee0fbbe7b9624fcb0be9f2c644eed2e10c752cd0
huy.bui <huy.bui@asiantech.vn>
2020-07-20 09:58:34 +0700

    update new day

--- app/src/at-huybui/java/com/asiantech/intern20summer1/fragment/SignUpFragment.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/fragment/SignUpFragment.kt
@@ -7,23 +7,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.util.PatternsCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.asiantech.intern20summer1.R
+import com.asiantech.intern20summer1.`class`.Account
 import com.asiantech.intern20summer1.fragment.SignInFragment.Companion.REGEX_PASSWORD
 import kotlinx.android.synthetic.`at-huybui`.fragment_sign_up.*
 
 class SignUpFragment : Fragment() {
     companion object {
         private val REGEX_NUMBER_PHONE = """^[0-9]{10}$""".toRegex()
+        private const val TICK_ICON: Int = 1
+        private const val ERROR_ICON: Int = 0
+        private const val HIDE_ICON: Int = -1
     }
 
+    private var toastStatus: Toast? = null
     private var emailBuffer = ""
     private var nameBuffer = ""
     private var numberPhoneBuffer = ""
     private var passwordBuffer = ""
+    private var rewritePassStatus = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,58 +47,227 @@ class SignUpFragment : Fragment() {
         handleForEmailEditText()
         handleForPasswordEditText()
         handleForNumberPhoneEditText()
-
+        handleForRewritePasswordEditText()
+        handleForRegisterButton()
+        handleForFullNameEditText()
     }
 
+    /**
+     * This function handle for back to sign in button
+     * Click back to sign in button well back to sign in fragment
+     */
     private fun handleForBackToSignInButton() {
         btnBackToSignIn.setOnClickListener {
             fragmentManager?.popBackStack()
         }
     }
 
+    /**
+     * This function will handle for name edit text
+     * It will show or hide tick icon follow text
+     */
     private fun handleForFullNameEditText() {
-
+        edtNameSingUp.addTextChangedListener { text ->
+            nameBuffer = if (edtNameSingUp.text.isNotEmpty()) {
+                setIconForEditText(edtNameSingUp, TICK_ICON)
+                text.toString()
+            } else {
+                setIconForEditText(edtNameSingUp, HIDE_ICON)
+                ""
+            }
+        }
     }
 
+    /**
+     * This function will handle for email edit text
+     * It will change tick or error icon follow text in box
+     */
     private fun handleForEmailEditText() {
         edtEmailSignUp.addTextChangedListener { text ->
             val pattern = PatternsCompat.EMAIL_ADDRESS.matcher(text.toString()).matches()
-            setIconForEditText(edtEmailSignUp, pattern)
-            emailBuffer = if (pattern) text.toString() else ""
+            emailBuffer = if (text.toString().isNotEmpty()) {
+                if (pattern) {
+                    setIconForEditText(edtEmailSignUp, TICK_ICON)
+                    text.toString()
+                } else {
+                    setIconForEditText(edtEmailSignUp, ERROR_ICON)
+                    ""
+                }
+            } else {
+                setIconForEditText(edtEmailSignUp, HIDE_ICON)
+                ""
+            }
         }
     }
 
+    /**
+     * This function will handle for number phone edit text
+     * It will change tick or error icon follow text in box
+     */
     private fun handleForNumberPhoneEditText() {
         edtNumberSignUp.addTextChangedListener { text ->
             val pattern = text.toString().matches(REGEX_NUMBER_PHONE)
-            setIconForEditText(edtNumberSignUp, pattern)
+            numberPhoneBuffer = if (text.toString().isNotEmpty()) {
+                if (pattern) {
+                    setIconForEditText(edtNumberSignUp, TICK_ICON)
+                    text.toString()
+                } else {
+                    setIconForEditText(edtNumberSignUp, ERROR_ICON)
+                    ""
+                }
+            } else {
+                setIconForEditText(edtNumberSignUp, HIDE_ICON)
+                ""
+            }
         }
-
     }
 
+    /**
+     * This function will handle for password edit text
+     * It will change tick or error icon follow text in box
+     * It also handle job of handle for rewrite password function
+     */
     private fun handleForPasswordEditText() {
         edtPasswordSignUp.addTextChangedListener { text ->
             val pattern = text.toString().matches(REGEX_PASSWORD)
-            setIconForEditText(edtPasswordSignUp, pattern)
-            passwordBuffer = if (pattern) text.toString() else ""
+            passwordBuffer = if (text.toString().isNotEmpty()) {
+                if (pattern) {
+                    setIconForEditText(edtPasswordSignUp, TICK_ICON)
+                    text.toString()
+                } else {
+                    setIconForEditText(edtPasswordSignUp, ERROR_ICON)
+                    ""
+                }
+            } else {
+                setIconForEditText(edtPasswordSignUp, HIDE_ICON)
+                ""
+            }
+            //job of handle function for rewrite password edit text view
+            rewritePassStatus = if (edtRePassSignUp.text.toString().isNotEmpty()) {
+                if (edtRePassSignUp.text.toString() == passwordBuffer) {
+                    setIconForEditText(edtRePassSignUp, TICK_ICON)
+                    true
+                } else {
+                    setIconForEditText(edtRePassSignUp, ERROR_ICON)
+                    false
+                }
+            } else {
+                setIconForEditText(edtRePassSignUp, HIDE_ICON)
+                false
+            }
         }
     }
 
+    /**
+     * This function will handle for rewrite password edit text
+     * It will change tick or error icon follow text in box
+     * It will check match or not match follow password
+     */
     private fun handleForRewritePasswordEditText() {
-
-    }
-
-    private fun handleForRegisterButton() {
-
-    }
-
-    @SuppressLint("NewApi")
-    private fun setIconForEditText(editText: EditText, boolean: Boolean) {
-        if (boolean) {
-            editText.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.icon_tick, 0)
-        } else {
-            editText.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.icon_error, 0)
+        edtRePassSignUp.addTextChangedListener { text ->
+            rewritePassStatus = if (text.toString().isNotEmpty()) {
+                if (text.toString() == passwordBuffer) {
+                    setIconForEditText(edtRePassSignUp, TICK_ICON)
+                    true
+                } else {
+                    setIconForEditText(edtRePassSignUp, ERROR_ICON)
+                    false
+                }
+            } else {
+                setIconForEditText(edtRePassSignUp, HIDE_ICON)
+                false
+            }
         }
+    }
 
+    /**
+     * This function handle for register button
+     * It check edit text boxes and show toast follow condition
+     */
+    private fun handleForRegisterButton() {
+        btnRegister.setOnClickListener {
+            when (true) {
+                edtNameSingUp.text.isEmpty() -> {
+                    edtNameSingUp.requestFocus()
+                    showToast(getString(R.string.enter_input_your_name))
+                }
+                edtEmailSignUp.text.isEmpty() -> {
+                    edtEmailSignUp.requestFocus()
+                    showToast(getString(R.string.enter_input_your_email))
+                }
+                edtNumberSignUp.text.isEmpty() -> {
+                    edtNumberSignUp.requestFocus()
+                    showToast(getString(R.string.enter_input_your_email))
+                }
+                edtPasswordSignUp.text.isEmpty() -> {
+                    edtPasswordSignUp.requestFocus()
+                    showToast("Enter input your password")
+                }
+                edtRePassSignUp.text.isEmpty() -> {
+                    edtRePassSignUp.requestFocus()
+                    showToast("Please, rewrite your password")
+                }
+                emailBuffer.isEmpty() -> {
+                    edtEmailSignUp.requestFocus()
+                    showToast("Invalid email")
+                }
+                numberPhoneBuffer.isEmpty() -> {
+                    edtNumberSignUp.requestFocus()
+                    showToast("Invalid number phone")
+                }
+                passwordBuffer.isEmpty() -> {
+                    edtPasswordSignUp.requestFocus()
+                    showToast("Invalid password")
+                }
+                !rewritePassStatus -> {
+                    edtRePassSignUp.requestFocus()
+                    showToast("Password does not match")
+                }
+                else -> {
+                    showToast("Register complete!")
+                    val account =
+                        Account(nameBuffer, emailBuffer, numberPhoneBuffer, passwordBuffer)
+                }
+            }
+        }
+    }
+
+    /**
+     * This function set icon for edit text views
+     * If argument is TICK_ICON, set tick icon for edit text view
+     * If argument is ERROR_ICON, set error icon for edit text view
+     * If argument is HIDE_ICON, hide icon of edit text view
+     * Argument had defined in companion
+     */
+    @SuppressLint("NewApi")
+    private fun setIconForEditText(editText: EditText, status: Int) {
+        when (status) {
+            TICK_ICON -> editText.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                0,
+                0,
+                R.drawable.icon_tick,
+                0
+            )
+            ERROR_ICON -> editText.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                0,
+                0,
+                R.drawable.icon_error,
+                0
+            )
+            HIDE_ICON -> editText.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                0,
+                0,
+                0,
+                0
+            )
+        }
+    }
+
+    /**
+     * This function will show a toast on display
+     */
+    private fun showToast(text: Any, duration: Int = Toast.LENGTH_SHORT) {
+        toastStatus?.cancel()
+        toastStatus = Toast.makeText(context, text.toString(), duration).apply { show() }
     }
 }
