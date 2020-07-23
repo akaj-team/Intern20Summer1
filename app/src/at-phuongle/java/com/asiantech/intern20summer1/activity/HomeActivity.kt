23b09896f3e8ce9400724697654fbb9de00d4817
Phuong Le T. D. [2] <phuong.le2@asiantech.vn>
2020-07-23 13:07:48 +0700

    handle ui activity home

---
+++ app/src/at-phuongle/java/com/asiantech/intern20summer1/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.asiantech.intern20summer1.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asiantech.intern20summer1.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
