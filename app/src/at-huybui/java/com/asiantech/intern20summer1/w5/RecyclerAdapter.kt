d47a357d1ffe606f606286d36059aa56c35a5901
huy.bui <huy.bui@asiantech.vn>
2020-07-24 09:03:26 +0700

    update recyclerAdapter

--- app/src/at-huybui/java/com/asiantech/intern20summer1/w5/RecyclerAdapter.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w5/RecyclerAdapter.kt
@@ -1,31 +1,36 @@
 package com.asiantech.intern20summer1.w5
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.asiantech.intern20summer1.R
+import kotlinx.android.synthetic.`at-huybui`.recycler_item.view.*
 
 
-class ListAdapter(private val mutableList: MutableList<ItemRecycler>) :
-    RecyclerView.Adapter<ItemViewHolder>() {
+class RecyclerAdapter(private val mutableList: MutableList<ItemRecycler>) :
+    RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        return ItemViewHolder(inflater, parent)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
+        return ItemViewHolder(itemView)
     }
 
     override fun getItemCount() = mutableList.size
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item: ItemRecycler = mutableList[position]
-        holder.bind
-    }
-}
-
-
-class ItemViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
-    RecyclerView.ViewHolder(inflater.inflate(R.layout.recycler_item, parent, false)) {
-    fun bind(item: ItemRecycler) {
+        val currentItem = mutableList[position]
+        holder.name1.text = currentItem.name
+        holder.name2.text = currentItem.name
     }
 
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var name1 = itemView.tvName1W5
+        var name2 = itemView.tvName2W5
+
+        fun bind(item: ItemRecycler) {
+        }
+    }
 }
--- app/src/at-huybui/java/com/asiantech/intern20summer1/w5/RecyclerViewActivity.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w5/RecyclerViewActivity.kt
@@ -2,11 +2,26 @@ package com.asiantech.intern20summer1.w5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.asiantech.intern20summer1.R
+import kotlinx.android.synthetic.`at-huybui`.activity_recycler_view.*
 
 class RecyclerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+
+        val exampleList:MutableList<ItemRecycler> = mutableListOf()
+
+        exampleList.add(ItemRecycler("huy",1,1,true,""))
+        exampleList.add(ItemRecycler("huy",1,1,true,""))
+        exampleList.add(ItemRecycler("huy",1,1,true,""))
+        exampleList.add(ItemRecycler("huy",1,1,true,""))
+
+        recycler_view.adapter = RecyclerAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.setHasFixedSize(true)
+
     }
 }
