2fb4c1246956dd34156f3f3c7d5a938e5e683436
at-longphan <long.phan@asiantech.vn>
2020-07-24 11:40:51 +0700

    init some files

---
+++ app/src/at-longphan/java/com/asiantech/intern20summer1/week5/activity/TimeLineActivity.kt
@@ -0,0 +1,12 @@
+package com.asiantech.intern20summer1.week5.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asiantech.intern20summer1.R
+
+class TimeLineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_line)
+    }
+}
---
+++ app/src/at-longphan/java/com/asiantech/intern20summer1/week5/model/TimeLineItem.kt
@@ -0,0 +1,5 @@
+package com.asiantech.intern20summer1.week5.model
+
+class TimeLineItem {
+
+}
