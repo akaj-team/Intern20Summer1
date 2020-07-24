d24296dba6215ff4545b4d37a53269f0157dfe3d
huy.bui <huy.bui@asiantech.vn>
2020-07-24 07:53:03 +0700

    update recyclerdapter

--- app/src/at-huybui/java/com/asiantech/intern20summer1/w5/RecyclerAdapter.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w5/RecyclerAdapter.kt
@@ -6,23 +6,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.asiantech.intern20summer1.R
 
 
-class ListAdapter(private val mutableList: MutableList<ItemRecycler>)
-    : RecyclerView.Adapter<MovieViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        TODO("Not yet implemented")
+class ListAdapter(private val mutableList: MutableList<ItemRecycler>) :
+    RecyclerView.Adapter<ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ItemViewHolder(inflater, parent)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = mutableList.size
 
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item: ItemRecycler = mutableList[position]
+        holder.bind
     }
 }
 
 
-class MovieViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
-    RecyclerView.ViewHolder(inflater.inflate(R.layout., parent, false)) {
+class ItemViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.recycler_item, parent, false)) {
+    fun bind(item: ItemRecycler) {
+    }
 
 }
