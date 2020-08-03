75fa4028be2594438fb9af42f8742681c1c494ae
at-linhle <linh.le@asiantech.vn>
2020-08-03 15:59:41 +0700

    add home activity to choose week

---
+++ app/src/at-linhle/java/com/asiantech/intern20summer1/home/HomeActivity.kt
@@ -0,0 +1,39 @@
+package com.asiantech.intern20summer1.home
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asiantech.intern20summer1.R
+import com.asiantech.intern20summer1.week3.MainActivity
+import com.asiantech.intern20summer1.week4.views.SplashActivity
+import com.asiantech.intern20summer1.week6.views.IndicatorActivity
+import kotlinx.android.synthetic.`at-linhle`.activity_container.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_container)
+        handleWeekThreeButtonClick()
+        handleWeekFourButtonClick()
+        handleWeekSixButtonClick()
+    }
+
+    private fun handleWeekThreeButtonClick() {
+        weekThree.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun handleWeekFourButtonClick() {
+        weekFour.setOnClickListener {
+            startActivity(Intent(this, SplashActivity::class.java))
+        }
+    }
+
+    private fun handleWeekSixButtonClick() {
+        weekSix.setOnClickListener {
+            startActivity(Intent(this, IndicatorActivity::class.java))
+        }
+    }
+}
