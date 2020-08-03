402cdbbaa40d0960cd40df973ad17208d2a3d25c
at-sonnguyen <son.nguyen@asiantech.vn>
2020-08-03 10:06:10 +0700

    modify at warning

--- app/src/at-sonnguyen/java/com/asiantech/intern20summer1/w6/data/Player.kt
+++ app/src/at-sonnguyen/java/com/asiantech/intern20summer1/w6/data/Player.kt
@@ -1,3 +1,3 @@
 package com.asiantech.intern20summer1.w6.data
 
-data class Player(val position: String?, val image : Int, val name : String?, val national : Int)
+data class Player( val image : Int, val national : Int)
--- app/src/at-sonnguyen/java/com/asiantech/intern20summer1/w6/fragment/HomeFragment.kt
+++ app/src/at-sonnguyen/java/com/asiantech/intern20summer1/w6/fragment/HomeFragment.kt
@@ -14,17 +14,17 @@ class HomeFragment : Fragment() {
 
     private var position = 0
     private var players = arrayListOf<Player>(
-        Player(activity?.getString(R.string.w6_position_Forward), R.drawable.img_martial, activity?.getString(R.string.w6_player_1), R.drawable.ic_france),
-        Player(activity?.getString(R.string.w6_position_Forward), R.drawable.img_marcus, activity?.getString(R.string.w6_player_2), R.drawable.ic_england),
-        Player(activity?.getString(R.string.w6_position_Forward), R.drawable.img_greenwood, activity?.getString(R.string.w6_player_3), R.drawable.ic_england),
-        Player(activity?.getString(R.string.w6_position_Midfield), R.drawable.img_bruno_fernandes, activity?.getString(R.string.w6_player_4), R.drawable.ic_portugal),
-        Player(activity?.getString(R.string.w6_position_Midfield), R.drawable.img_pogba_2, activity?.getString(R.string.w6_player_5), R.drawable.ic_france),
-        Player(activity?.getString(R.string.w6_position_Midfield), R.drawable.img_matic, activity?.getString(R.string.w6_player_6), R.drawable.ic_serbia),
-        Player(activity?.getString(R.string.w6_position_Defender), R.drawable.img_luke_shaw, activity?.getString(R.string.w6_player_7), R.drawable.ic_england),
-        Player(activity?.getString(R.string.w6_position_Defender), R.drawable.img_harry_maguire, activity?.getString(R.string.w6_player_8), R.drawable.ic_england),
-        Player(activity?.getString(R.string.w6_position_Defender), R.drawable.img_lindelof, activity?.getString(R.string.w6_player_9), R.drawable.ic_sweden),
-        Player(activity?.getString(R.string.w6_position_Defender), R.drawable.img_wanbi, activity?.getString(R.string.w6_player_10), R.drawable.ic_england),
-        Player(activity?.getString(R.string.w6_position_Goalkeeper), R.drawable.img_de_gea, activity?.getString(R.string.w6_player_11), R.drawable.ic_spain)
+        Player( R.drawable.img_martial,  R.drawable.ic_france),
+        Player(R.drawable.img_marcus,  R.drawable.ic_england),
+        Player( R.drawable.img_greenwood, R.drawable.ic_england),
+        Player( R.drawable.img_bruno_fernandes,  R.drawable.ic_portugal),
+        Player( R.drawable.img_pogba_2,  R.drawable.ic_france),
+        Player(R.drawable.img_matic,  R.drawable.ic_serbia),
+        Player( R.drawable.img_luke_shaw, R.drawable.ic_england),
+        Player(R.drawable.img_harry_maguire, R.drawable.ic_england),
+        Player(R.drawable.img_lindelof,  R.drawable.ic_sweden),
+        Player(R.drawable.img_wanbi, R.drawable.ic_england),
+        Player(R.drawable.img_de_gea,R.drawable.ic_spain)
     )
 
     companion object {
@@ -58,8 +58,6 @@ class HomeFragment : Fragment() {
     private fun loadData(){
         val player = players[Random.nextInt(1, players.size)]
         imgHomeFragment.setImageResource(player.image)
-        tvPosition.text = player.position
-        tvName.text = player.name
         imgNational.setImageResource(player.national)
     }
 }
