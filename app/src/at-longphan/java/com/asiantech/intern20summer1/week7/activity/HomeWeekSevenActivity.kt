126dbf7084330636ddf2d6580a4f294b9fbc5e4f
at-longphan <long.phan@asiantech.vn>
2020-08-08 16:11:36 +0700

    add views

---
+++ app/src/at-longphan/java/com/asiantech/intern20summer1/week7/activity/HomeWeekSevenActivity.kt
@@ -0,0 +1,12 @@
+package com.asiantech.intern20summer1.week7.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asiantech.intern20summer1.R
+
+class HomeWeekSevenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_w7)
+    }
+}
