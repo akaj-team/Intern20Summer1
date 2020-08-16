abea964560d78ff6adb99b011632c18081603fc1
at-huybui <huy.bui@asiantech.vn>
2020-08-16 16:08:03 +0700

    add download activity

---
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w8/DownLoadActivity.kt
@@ -0,0 +1,12 @@
+package com.asiantech.intern20summer1.w8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.asiantech.intern20summer1.R
+
+class DownLoadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_down_load)
+    }
+}
