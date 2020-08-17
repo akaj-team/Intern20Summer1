4e3e4cb30ddae9486ad5aaacbcc27a7fc2ec38c5
at-vuongphan <vuong.phan@asiantech.vn>
2020-08-17 16:53:44 +0700

    change detail fragment

--- app/src/at-vuongphan/java/com/asiantech/intern20summer1/fragment/VegetableDetailFragment.kt
+++ app/src/at-vuongphan/java/com/asiantech/intern20summer1/fragment/VegetableDetailFragment.kt
@@ -1,14 +1,18 @@
 package com.asiantech.intern20summer1.fragment
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.asiantech.intern20summer1.R
 import com.asiantech.intern20summer1.database.Cultivation
 import com.asiantech.intern20summer1.database.Plant
 import com.asiantech.intern20summer1.database.VegetableDB
+import kotlinx.android.synthetic.`at-vuongphan`.w7_vegetable_detail.*
 
 class VegetableDetailFragment : Fragment() {
     private var dataBase: VegetableDB? = null
@@ -32,5 +36,45 @@ class VegetableDetailFragment : Fragment() {
     ): View? {
         dataBase = VegetableDB.dataBaseConnect(requireContext())
         return inflater.inflate(R.layout.w7_vegetable_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initListener()
+    }
+
+    private fun initListener() {
+        initBack()
+        initButtonDelete()
+        initForButtonSprinklers()
+    }
+
+    private fun initBack() {
+        imgBackDetail?.setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+    }
+
+    private fun initButtonDelete() {
+        btnCutTree?.setOnClickListener {
+            Toast.makeText(context, R.string.clear_plant_complete, Toast.LENGTH_SHORT).show()
+            fragmentManager?.popBackStack()
+        }
+    }
+
+    private fun initForButtonSprinklers() {
+        btnSprinklers?.setOnClickListener {
+            Toast.makeText(context, R.string.watering_plant_complete, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initView() {
+        cultivation = dataBase?.cultivationDao()?.getCultivation(arguments?.getInt(KEY_POS_ID))
+        plant = dataBase?.plantDao()?.getPlant(cultivation?.plantId)
+        imgAppBarLayout?.setImageURI(Uri.parse(plant?.imageUri))
+        val textCultivation = getString(R.string.text_cultivation, cultivation?.dateCultivation)
+        val textWater = getString(R.string.text_watering, cultivation?.dateWatering)
     }
 }
