052d2d5f8ff9a62f094d73f3900d76d3185787de
at-huybui <huy.bui@asiantech.vn>
2020-08-18 15:18:37 +0700

    finish asynctask

--- app/src/at-huybui/java/com/asiantech/intern20summer1/w8/DownLoadActivity.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w8/DownLoadActivity.kt
@@ -1,8 +1,11 @@
 package com.asiantech.intern20summer1.w8
 
-import android.os.AsyncTask
-import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+import android.os.*
+import android.os.Environment.getExternalStoragePublicDirectory
 import android.util.Log.d
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.asiantech.intern20summer1.R
 import kotlinx.android.synthetic.`at-huybui`.activity_down_load.*
@@ -12,7 +15,14 @@ import java.io.OutputStream
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
+
 class DownLoadActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REGEX_URL =
+            """(https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?"""
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_down_load)
@@ -21,78 +31,159 @@ class DownLoadActivity : AppCompatActivity() {
 
     private fun initListener() {
         btnAsyncTask?.setOnClickListener {
-
-            /**
-             * https://r8---sn-8pxuuxa-q5q6.googlevideo.com/videoplayback?expire=1597701785&ei=Oao6X9btJJq24QKm-664CA&ip=2402%3A800%3A621c%3Aeb44%3A304f%3A3449%3Ab8da%3A6e8b&id=o-APssU5WYP9mLlSxDmGjZsheHoEbkzVc0ksNfLD-OSmq5&itag=22&source=youtube&requiressl=yes&mh=II&mm=31%2C29&mn=sn-8pxuuxa-q5q6%2Csn-npoeenl7&ms=au%2Crdu&mv=m&mvi=8&pcm2cms=yes&pl=48&gcr=vn&initcwndbps=711250&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=2814.049&lmt=1595124863041265&mt=1597680046&fvip=3&fexp=23883098&c=WEB&txp=5535432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRAIgSajLEjIy1C1bmxFCbIzkecZwaKT-yyjOuyrrUcXDPG4CIEewZ236IJNS0l-NrrFtFxC2f8lEvATWbyo51dX8ZwKR&sig=AOq0QJ8wRgIhALdusV0jom4wDtE27tMECJNsW13RVG1RSx0FpgFXa5-DAiEAlPkS4_VkfgwLAYZ_Dxfp-JrZy2ljr1BMoH5q7RJcnvo%3D
-             */
             val url = edtUrl.text.toString()
             val name = edtNameFile.text.toString()
             val extension = edtExtension.text.toString()
-            DownLoadAsyncTask().execute(url, name, extension)
+            if (name.isEmpty()) {
+                Toast.makeText(this@DownLoadActivity, "Hãy nhập tên file", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (extension.isEmpty()) {
+                Toast.makeText(this@DownLoadActivity, "Hãy nhập extension", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (!url.matches(REGEX_URL.toRegex())) {
+                Toast.makeText(this@DownLoadActivity, "Url không hợp lệ", Toast.LENGTH_SHORT).show()
+            } else {
+                DownLoadAsyncTask().execute(url, name, extension)
+            }
+        }
+
+        btnOpenFile?.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            val directory = Environment.DIRECTORY_DOWNLOADS
+            d("XXXX", "[path]: $directory")
+            val uri: Uri = Uri.parse(directory) // a directory
+            intent.setDataAndType(uri, "*/*")
+            startActivity(intent)
         }
     }
 
-    inner class DownLoadAsyncTask : AsyncTask<String, Int, String>() {
-        override fun doInBackground(vararg params: String?): String? {
+    inner class DownLoadAsyncTask : AsyncTask<String, Int, Int>() {
+        @Suppress("DEPRECATION")
+        override fun doInBackground(vararg params: String?): Int? {
             val outputStream: OutputStream
             try {
                 val url = URL(params[0])
                 val nameFile = params[1]
                 val extension = params[2]
-                d("XXXX", " URL ${params[0]}")
                 val connection = url.openConnection() as HttpsURLConnection
                 connection.connect()
-                val input = connection.inputStream
-                val directory = getExternalFilesDir(null)
-                val path = File(directory, "$nameFile$extension")
-                outputStream = FileOutputStream("${path}")
-                d("XXXX", " Uri ${path}")
                 val length = connection.contentLength
-                d("XXXX", length.toString())
                 if (length > 0) {
+                    val inputStream = connection.inputStream
+                    val directory =
+                        getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                    val path = File(directory, "$nameFile$extension")
+                    outputStream = FileOutputStream("$path")
                     val data = ByteArray(length)
-                    d("XXXX", length.toString())
                     if (connection.responseCode != HttpsURLConnection.HTTP_OK) {
-                        d("XXXX", "   return HTTP")
-                        return "Server return HTTP" + (connection.responseCode
-                            .toString() + " " + connection.responseMessage)
+                        return 0
                     }
-                    var count = input?.read(data)
                     var total = 0
+                    var count = inputStream?.read(data)
+                    d("XXXX", "cacth ae")
+                    if (count == -1) {
+                        return -1
+                    }
                     while (count != -1) {
                         // allow canceling with back button
                         if (isCancelled) {
-                            d("XXXX", "[file] close")
-                            input?.close()
+                            inputStream?.close()
                             return null
                         }
-                        if (count != null) {
-                            total += count
+                        count?.let {
+                            total += it
+                            outputStream.write(data, 0, it)
                         }
                         // publishing the progress....
-                        d("XXXX", "[async]: total = $total\t\t: leng = $length")
-                        publishProgress(((total/ length.toFloat()) * 100).toInt())
-                        count?.let { outputStream.write(data, 0, it) }
-                        count = input?.read(data)
+                        publishProgress(((total / length.toFloat()) * 100).toInt())
+                        count = inputStream?.read(data)
                     }
                 }
             } catch (nfe: NumberFormatException) {
                 nfe.printStackTrace()
-                d("XXXX", "[Catch]: Tính toán số học")
+                d("XXXX", "cacth nfe")
             } catch (ae: ArithmeticException) {
+                d("XXXX", "cacth ae")
                 ae.printStackTrace()
-                d("XXXX", "[Catch]:Không phải số")
             } catch (ie: IllegalArgumentException) {
+                d("XXXX", "cacth ie")
                 ie.printStackTrace()
-                d("XXXX", "[Catch]: Đối số không phù hợp")
             }
-            return null
+            return -1
         }
 
         override fun onProgressUpdate(vararg values: Int?) {
             super.onProgressUpdate(*values)
-            values[0]?.let { progressBar?.progress = it }
-            tvProgress?.text = "${values[0]}%"
+            values[0]?.let {
+                progressBar?.progress = it
+                val text = "$it%"
+                tvProgress?.text = text
+            }
+        }
+
+        override fun onPostExecute(result: Int?) {
+            super.onPostExecute(result)
+            when (result) {
+                0 -> {
+                    val text = getString(R.string.w8_server_not_ok)
+                    Toast.makeText(this@DownLoadActivity, text, Toast.LENGTH_SHORT).show()
+                }
+                -1 -> {
+                    d("XXXX", "dont download")
+                    val text = getString(R.string.w8_dont_download)
+                    Toast.makeText(this@DownLoadActivity, text, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private class UiHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+
+        }
+    }
+
+    private class MyHandlerThread(private var uiHandler: DownLoadActivity.UiHandler) :
+        HandlerThread("") {
+        var handler: Handler? = null
+
+        override fun onLooperPrepared() {
+            super.onLooperPrepared()
+            handler = getHandler(looper)
+        }
+
+        override fun run() {
+            super.run()
+            orderHandlerThread.sendOrder()
+            //4
+            try {
+                Thread.sleep(1000)
+            } catch (exception: InterruptedException) {
+                exception.printStackTrace()
+            }
+        }
+
+        private fun getHandler(looper: Looper): Handler {
+            //1
+            return object : Handler(looper) {
+                //2
+                override fun handleMessage(msg: Message) {
+                    super.handleMessage(msg)
+                    //6
+                    val processedMessage = Message()
+                    uiHandler.sendMessage(processedMessage)
+                }
+            }
+        }
+
+        fun sendOrder() {
+            //1
+            val message = Message()
+            //2
+            message.obj = foodOrder
+            //3
+            handler?.sendMessage(message)
         }
     }
 }
