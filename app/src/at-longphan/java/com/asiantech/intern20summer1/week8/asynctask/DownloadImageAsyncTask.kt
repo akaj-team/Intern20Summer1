b004293c353e4682d89e2b0bbe20b69e146f35f2
at-longphan <long.phan@asiantech.vn>
2020-08-21 14:47:28 +0700

    add try catch

--- app/src/at-longphan/java/com/asiantech/intern20summer1/week8/asynctask/DownloadImageAsyncTask.kt
+++ app/src/at-longphan/java/com/asiantech/intern20summer1/week8/asynctask/DownloadImageAsyncTask.kt
@@ -6,11 +6,8 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import com.asiantech.intern20summer1.R
-import com.asiantech.intern20summer1.week8.handler.DownloadImageHandler
-import java.io.BufferedInputStream
-import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
+import java.io.*
+import java.net.MalformedURLException
 import java.net.URL
 import java.util.*
 
@@ -20,34 +17,47 @@ class DownloadImageAsyncTask(
     private val tvDisplayCurrentProgress: TextView
 ) : AsyncTask<String, String, String>() {
 
-    companion object{
+    companion object {
         private const val HUNDRED = 100
+        private const val FILE_SIZE = 1024
+        private const val BUFFER_SIZE = 8192
     }
 
     override fun onPreExecute() {
         super.onPreExecute()
-        Toast.makeText(context, context.getString(R.string.toast_starting_download_w8), Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            context,
+            context.getString(R.string.toast_starting_download_w8),
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun doInBackground(vararg params: String?): String {
-        var count: Int
-        var total = 0
-        val url = URL(params[0])
-        val connection = url.openConnection()
-        connection.connect()
-        val lengthFile = connection.contentLength
-        val input: InputStream = BufferedInputStream(url.openStream(), DownloadImageHandler.BUFFER_SIZE)
-        val output =
-            FileOutputStream(File("${context.getExternalFilesDir(null)}/${Date().time}.jpg"))
-        val data = ByteArray(DownloadImageHandler.FILE_SIZE)
-        while (input.read(data).also { count = it } != -1) {
-            total += count
-            output.write(data, 0, count)
-            publishProgress("${(total * HUNDRED) / lengthFile}")
+        try {
+            var count: Int
+            var total = 0
+            val url = URL(params[0])
+            val connection = url.openConnection()
+            connection.connect()
+            val lengthFile = connection.contentLength
+            val input: InputStream = BufferedInputStream(url.openStream(), BUFFER_SIZE)
+            val output =
+                FileOutputStream(File("${context.getExternalFilesDir(null)}/${Date().time}.jpg"))
+            val data = ByteArray(FILE_SIZE)
+            while (input.read(data).also { count = it } != -1) {
+                total += count
+                output.write(data, 0, count)
+
+                publishProgress("${(total * HUNDRED) / lengthFile}")
+            }
+            output.flush()
+            output.close()
+            input.close()
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
-        output.flush()
-        output.close()
-        input.close()
         return ""
     }
 
@@ -61,6 +71,10 @@ class DownloadImageAsyncTask(
 
     override fun onPostExecute(result: String?) {
         super.onPostExecute(result)
-        Toast.makeText(context, context.getString(R.string.toast_finish_download_w8), Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            context,
+            context.getString(R.string.toast_finish_download_w8),
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
--- app/src/at-longphan/java/com/asiantech/intern20summer1/week8/handler/DownloadImageHandler.kt
+++ app/src/at-longphan/java/com/asiantech/intern20summer1/week8/handler/DownloadImageHandler.kt
@@ -3,10 +3,8 @@ package com.asiantech.intern20summer1.week8.handler
 import android.content.Context
 import android.os.Handler
 import android.os.Message
-import java.io.BufferedInputStream
-import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
+import java.io.*
+import java.net.MalformedURLException
 import java.net.URL
 import java.util.*
 
@@ -18,34 +16,41 @@ class DownloadImageHandler(
 
     companion object {
         internal const val DOWNLOAD_IMAGE_MSG_CODE = 3508
-        internal const val FILE_SIZE = 1024
-        internal const val BUFFER_SIZE = 8192
+        private const val FILE_SIZE = 1024
+        private const val BUFFER_SIZE = 8192
         private const val HUNDRED = 100
     }
 
     override fun run() {
-        var count: Int
-        var total = 0
-        val url = URL(urlString)
-        val connection = url.openConnection()
-        connection.connect()
+        try {
+            var count: Int
+            var total = 0
+            val url = URL(urlString)
+            val connection = url.openConnection()
+            connection.connect()
 
-        val lengthOfFile = connection.contentLength
-        val input: InputStream = BufferedInputStream(url.openStream(), BUFFER_SIZE)
-        val output =
-            FileOutputStream(File("${context.getExternalFilesDir(null)}/${Date().time}.jpg"))
-        val data = ByteArray(FILE_SIZE)
-        while (input.read(data).also { count = it } != -1) {
-            total += count
-            output.write(data, 0, count)
-            val process = (total * HUNDRED) / lengthOfFile
-            val msg = Message()
-            msg.what = DOWNLOAD_IMAGE_MSG_CODE
-            msg.arg1 = process
-            handler.sendMessage(msg)
+            val lengthOfFile = connection.contentLength
+            val input: InputStream = BufferedInputStream(url.openStream(), BUFFER_SIZE)
+            val output =
+                FileOutputStream(File("${context.getExternalFilesDir(null)}/${Date().time}.jpg"))
+            val data = ByteArray(FILE_SIZE)
+            while (input.read(data).also { count = it } != -1) {
+                total += count
+                output.write(data, 0, count)
+
+                val process = (total * HUNDRED) / lengthOfFile
+                val msg = Message()
+                msg.what = DOWNLOAD_IMAGE_MSG_CODE
+                msg.arg1 = process
+                handler.sendMessage(msg)
+            }
+            output.flush()
+            output.close()
+            input.close()
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
-        output.flush()
-        output.close()
-        input.close()
     }
 }
