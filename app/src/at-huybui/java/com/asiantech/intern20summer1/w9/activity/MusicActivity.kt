4bcb69e9574a6a6138900e7002491a2068763213
at-huybui <huy.bui@asiantech.vn>
2020-08-21 10:39:22 +0700

    add fragment splash

--- app/src/at-huybui/java/com/asiantech/intern20summer1/w9/activity/MusicActivity.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w9/activity/MusicActivity.kt
@@ -2,6 +2,13 @@ package com.asiantech.intern20summer1.w9.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asiantech.intern20summer1.R
+
+/**
+ * Asian Tech Co., Ltd.
+ * Created by at-huybui on 08/21/20
+ * This is activity class for main activity of music application
+ */
 
 class MusicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w9/fragment/SplashFragment.kt
@@ -0,0 +1,6 @@
+package com.asiantech.intern20summer1.w9.fragment
+
+import androidx.fragment.app.Fragment
+
+class SplashFragment : Fragment() {
+}
