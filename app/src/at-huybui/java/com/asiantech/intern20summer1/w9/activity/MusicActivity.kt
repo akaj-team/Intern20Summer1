6672d965277c9f3115d4125a1001d8f537d610fd
at-huybui <huy.bui@asiantech.vn>
2020-08-21 10:35:30 +0700

    add MuusicActivity

---
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w9/activity/MusicActivity.kt
@@ -0,0 +1,11 @@
+package com.asiantech.intern20summer1.w9.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music)
+    }
+}
