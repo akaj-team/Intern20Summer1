d39ad16e261d09fc356413c3f5eac92451517ba9
at-huybui <huy.bui@asiantech.vn>
2020-08-21 11:19:41 +0700

    handle open splash

--- app/src/at-huybui/java/com/asiantech/intern20summer1/w9/activity/MusicActivity.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w9/activity/MusicActivity.kt
@@ -1,8 +1,10 @@
 package com.asiantech.intern20summer1.w9.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.asiantech.intern20summer1.R
+import com.asiantech.intern20summer1.w9.fragment.SplashFragment
 
 /**
  * Asian Tech Co., Ltd.
@@ -14,5 +16,21 @@ class MusicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music)
+
+        handleReplaceFragment(SplashFragment.newInstance(), false)
+    }
+
+    internal fun handleReplaceFragment(
+        fragment: Fragment,
+        isBackStack: Boolean = false,
+        nameBackStack: String = ""
+    ) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.containerMain, fragment)
+        if (isBackStack) {
+            fragmentTransaction.addToBackStack(nameBackStack)
+        }
+        fragmentTransaction.commit()
     }
 }
--- app/src/at-huybui/java/com/asiantech/intern20summer1/w9/fragment/SplashFragment.kt
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w9/fragment/SplashFragment.kt
@@ -1,7 +1,29 @@
 package com.asiantech.intern20summer1.w9.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.asiantech.intern20summer1.R
+
+/**
+ * Asian Tech Co., Ltd.
+ * Created by at-huybui on 08/21/20
+ * This is fragment class for splash fragment of music application
+ */
 
 class SplashFragment : Fragment() {
 
+    companion object {
+        internal fun newInstance() = SplashFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.w9_fragment_splash, container, false)
+    }
 }
