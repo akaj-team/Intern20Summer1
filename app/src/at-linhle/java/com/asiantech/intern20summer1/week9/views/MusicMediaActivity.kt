eb3c2143795727627ae290966d7c6c757e149b4c
at-linhle <linh.le@asiantech.vn>
2020-08-23 16:26:50 +0700

    first commit

---
+++ app/src/at-linhle/java/com/asiantech/intern20summer1/week9/views/MusicMediaActivity.kt
@@ -0,0 +1,12 @@
+package com.asiantech.intern20summer1.week9.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asiantech.intern20summer1.R
+
+class MusicMediaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_media)
+    }
+}
