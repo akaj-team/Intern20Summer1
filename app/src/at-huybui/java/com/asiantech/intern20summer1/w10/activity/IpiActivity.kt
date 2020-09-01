8dad91a5169d9de90735f4f5294df615cbc49b1f
at-huybui <huy.bui@asiantech.vn>
2020-09-01 10:38:53 +0700

    add fragment

---
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w10/activity/IpiActivity.kt
@@ -0,0 +1,12 @@
+package com.asiantech.intern20summer1.w10.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.asiantech.intern20summer1.R
+
+class IpiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ipi)
+    }
+}
---
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w10/fragment/SignUpFragment.kt
@@ -0,0 +1,19 @@
+package com.asiantech.intern20summer1.w10.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.asiantech.intern20summer1.R
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.w10_fragment_sign_up,container,false)
+    }
+}
