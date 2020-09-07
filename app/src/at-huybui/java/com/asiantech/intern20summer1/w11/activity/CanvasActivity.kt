b6078eb721cd1b6b79d5fd31d4b7db5f2419e5a5
at-huybui <huy.bui@asiantech.vn>
2020-09-07 10:00:42 +0700

    add main Activity

---
+++ app/src/at-huybui/java/com/asiantech/intern20summer1/w11/activity/CanvasActivity.kt
@@ -0,0 +1,20 @@
+package com.asiantech.intern20summer1.w11.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.asiantech.intern20summer1.R
+
+/**
+ * Asian Tech Co., Ltd.
+ * Intern20Summer1 Project.
+ * Created by at-huybui on 07/09/2020.
+ * This is xxx TODO("Not yet implemented").
+ * It will TODO("Not yet implemented")
+ */
+
+class CanvasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.w11_activity_canvas)
+    }
+}
