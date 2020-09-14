eabc113a397ace79e566b33154ae1637696b3ee3
Hoang <hoang.tran3@asiantech.vn>
2020-09-14 11:17:28 +0700

    fix chart

--- app/src/at-hoangtran/java/com/asiantech/intern20summer1/week11/WeightDataView.kt
+++ app/src/at-hoangtran/java/com/asiantech/intern20summer1/week11/WeightDataView.kt
@@ -1,12 +1,13 @@
 package com.asiantech.intern20summer1.week11
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.DashPathEffect
-import android.graphics.Paint
+import android.graphics.*
+import android.os.Build
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import androidx.annotation.RequiresApi
 import kotlin.random.Random
 
 class WeightDataView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
@@ -16,7 +17,7 @@ class WeightDataView(context: Context, attributeSet: AttributeSet) : View(contex
         private const val DOT_COLOR = Color.BLUE
         private const val MAX_WEIGHT = 120
         private const val MIN_WEIGHT = 50
-        private const val MONTH = 12
+        private const val MONTH: Int = 12
     }
 
     private var sizeX = 0f
@@ -27,36 +28,71 @@ class WeightDataView(context: Context, attributeSet: AttributeSet) : View(contex
     private var linePaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var dotPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var dashPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var moveX = 0f
+    private var startMove = 0f
+    private var stopMove = 0f
+    private var distanceMove = 0f
+    private var oldDistance = 0f
+    private var path = Path()
+    private var isAddData = false
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
         initSize()
-        initData()
+        if (!isAddData) {
+            initData()
+            isAddData = true
+        }
         initPaint()
         drawAxis(canvas)
         drawAxisText(canvas)
         drawDot(canvas)
     }
 
-    private fun initSize() {
-        sizeX = width / 13.toFloat()
-        sizeY = height / 12.toFloat()
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                initData()
+                startMove = x
+            }
+            MotionEvent.ACTION_MOVE -> {
+                moveX = x
+                stopMove = x
+                distanceMove = startMove - stopMove
+                invalidate()
+                path.reset()
+            }
+            MotionEvent.ACTION_UP -> {
+                oldDistance += distanceMove
+            }
+        }
+        return true
     }
 
+    private fun initSize() {
+        sizeX = width / (MONTH + 1).toFloat()
+        sizeY = height / MONTH.toFloat()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     private fun initPaint() {
         linePaint.color = LINE_COLOR
         linePaint.style = Paint.Style.STROKE
         linePaint.strokeWidth = STROKE_WIDTH
         dotPaint.color = DOT_COLOR
         dashPaint.color = LINE_COLOR
+        setLayerPaint(dashPaint)
         dashPaint.style = Paint.Style.STROKE
         dashPaint.strokeWidth = STROKE_WIDTH
         dashPaint.pathEffect = DashPathEffect(floatArrayOf(10f, 10f), 0f)
     }
 
     private fun initData() {
-        for (i in 0..13) {
+        for (i in 0..MONTH) {
             weightData.add(Random.nextInt(MIN_WEIGHT, MAX_WEIGHT))
         }
     }
@@ -70,10 +106,7 @@ class WeightDataView(context: Context, attributeSet: AttributeSet) : View(contex
         canvas.drawText("Weight", d0 + 20f, d0 + 30f, textPaint)
         canvas.drawText("Month", d0 + 50f, height - sizeY * 4 + 30f, textPaint)
 
-        for (i in 1..MONTH) {
-            canvas.drawText(i.toString(), sizeX * i + 50f, height - sizeY * 4 + 30f, textPaint)
-        }
-        for (i in 5..13) {
+        for (i in MIN_WEIGHT / 10..(MAX_WEIGHT + 1) / 10) {
             canvas.drawText(
                 (i * 10).toString(),
                 d0,
@@ -85,41 +118,42 @@ class WeightDataView(context: Context, attributeSet: AttributeSet) : View(contex
 
     private fun drawDot(canvas: Canvas) {
         val dy = height - sizeY * 4
+        var dx = -distanceMove - oldDistance + sizeX + 50f
         val weightUnit = dy / (MAX_WEIGHT + 10 - MIN_WEIGHT)
-        for (i in 0..11) {
+        for (i in 0 until MONTH) {
+            val startX = dx
+            val startY = dy - (weightData[i] - MIN_WEIGHT) * weightUnit
+            val endX = dx + sizeX
+            val endY = dy - (weightData[i + 1] - MIN_WEIGHT) * weightUnit
             canvas.drawCircle(
-                sizeX * (i + 1) + 50f,
-                dy - (weightData[i] - MIN_WEIGHT) * weightUnit,
+                startX,
+                startY,
                 10f,
                 dotPaint
             )
-
             canvas.drawText(
                 weightData[i].toString(),
-                sizeX * (i + 1) + 40f,
-                dy - (weightData[i] - MIN_WEIGHT) * weightUnit - 15f,
+                startX - 8f,
+                startY - 15f,
                 textPaint
             )
-            canvas.drawLine(
-                sizeX * (i + 1) + 50f,
-                dy - (weightData[i] - MIN_WEIGHT) * weightUnit,
-                d0 + 50f, dy - (weightData[i] - MIN_WEIGHT) * weightUnit, dashPaint
-            )
-            canvas.drawLine(
-                sizeX * (i + 1) + 50f,
-                dy - (weightData[i] - MIN_WEIGHT) * weightUnit,
-                sizeX * (i + 1) + 50f,
-                dy, dashPaint
-            )
-            if (i > 0) {
+            canvas.drawText(i.toString(), startX, dy + 30f, textPaint)
+            path.moveTo(startX, startY)
+            path.lineTo(d0 + 50f, startY)
+            canvas.drawPath(path, dashPaint)
+            path.moveTo(startX, startY)
+            path.lineTo(startX, dy)
+            canvas.drawPath(path, dashPaint)
+            if (i != MONTH - 1) {
                 canvas.drawLine(
-                    sizeX * (i + 1) + 50f,
-                    dy - (weightData[i] - MIN_WEIGHT) * weightUnit,
-                    sizeX * (i) + 50f,
-                    dy - (weightData[i - 1] - MIN_WEIGHT) * weightUnit,
+                    startX,
+                    startY,
+                    endX,
+                    endY,
                     linePaint
                 )
             }
+            dx += sizeX
         }
     }
 }
