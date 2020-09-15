553646e4e66b1fc0f9dd95571d7c92c1bd2538e0
at-linhle <linh.le@asiantech.vn>
2020-09-15 09:12:25 +0700

    add handle ui login + register

--- app/src/at-linhle/java/com/asiantech/intern20summer1/week12/fragments/LoginFragment.kt
+++ app/src/at-linhle/java/com/asiantech/intern20summer1/week12/fragments/LoginFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.asiantech.intern20summer1.R
 import kotlinx.android.synthetic.`at-linhle`.fragment_login.*
+import java.util.regex.Pattern
 
 class LoginFragment : Fragment() {
 
     companion object {
+        internal const val MAX_EMAIL_LENGTH = 264
         internal fun newInstance() = LoginFragment()
     }
+
+    private val passwordPattern = Pattern.compile("""^(?=.*).{8,16}$""")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,14 +30,59 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         handleClickingRegisterTextView()
+        handleLoginEmailTextChanged()
+        handleLoginPasswordTextChanged()
+    }
 
+    private fun isSignUpPasswordValid(password: String) =
+        passwordPattern.matcher(password).matches()
+
+    private fun isSignUpEmailValid(email: String) =
+        android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+            .matches() && email.length <= MAX_EMAIL_LENGTH
+
+    private fun isCorrectFormat(
+        email: String,
+        password: String
+    ) = isSignUpEmailValid(email) && isSignUpPasswordValid(password)
+
+    private fun setBackGroundLoginButton() {
+        if (btnLogin.isEnabled) {
+            btnLogin?.setBackgroundResource(R.drawable.bg_button_enabled)
+        } else {
+            btnLogin?.setBackgroundResource(R.drawable.bg_button_disabled)
+        }
+    }
+
+    private fun handleLoginEmailTextChanged() {
+        edtEmail?.addTextChangedListener(onTextChanged = { p0: CharSequence?, _, _, _ ->
+            btnLogin?.isEnabled = isCorrectFormat(
+                p0.toString(),
+                edtPassword.text.toString()
+            )
+            setBackGroundLoginButton()
+        })
+    }
+
+    private fun handleLoginPasswordTextChanged() {
+        edtPassword?.addTextChangedListener(onTextChanged = { p0: CharSequence?, _, _, _ ->
+            btnLogin?.isEnabled = isCorrectFormat(
+                edtEmail.text.toString(),
+                p0.toString()
+            )
+            setBackGroundLoginButton()
+        })
     }
 
     private fun handleClickingRegisterTextView() {
         tvRegister.setOnClickListener {
             val transaction = fragmentManager?.beginTransaction()
-            transaction?.add(R.id.flLoginContainer, RegisterFragment.newInstance())
-                ?.addToBackStack(null)?.hide(this)?.commit()
+            transaction?.add(R.id.flLoginContainer, RegisterFragment.newInstance().apply {
+                onRegisterSuccess = { email, password ->
+                    this@LoginFragment.edtEmail.setText(email)
+                    this@LoginFragment.edtPassword.setText(password)
+                }
+            })?.addToBackStack(null)?.hide(this)?.commit()
         }
     }
 }
--- app/src/at-linhle/java/com/asiantech/intern20summer1/week12/fragments/RegisterFragment.kt
+++ app/src/at-linhle/java/com/asiantech/intern20summer1/week12/fragments/RegisterFragment.kt
@@ -4,19 +4,100 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.asiantech.intern20summer1.R
+import com.asiantech.intern20summer1.week12.fragments.LoginFragment.Companion.MAX_EMAIL_LENGTH
+import kotlinx.android.synthetic.`at-linhle`.fragment_register.*
+import java.util.regex.Pattern
 
-class RegisterFragment: Fragment() {
+class RegisterFragment : Fragment() {
 
-    companion object{
+    companion object {
+        private const val MAX_FULL_NAME_LENGTH = 64
         internal fun newInstance() = RegisterFragment()
     }
+
+    internal var onRegisterSuccess: (email: String, password: String) -> Unit = { _, _ -> }
+    private val passwordPattern = Pattern.compile("""^(?=.*).{8,16}$""")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleClickingArrowBack()
+        handleRegisterEmailTextChanged()
+        handleRegisterFullNameTextChanged()
+        handleRegisterPasswordTextChanged()
+    }
+
+    private fun isSignUpFullNameValid(fullName: String) = fullName.length <= MAX_FULL_NAME_LENGTH
+
+    private fun isSignUpPasswordValid(password: String) =
+        passwordPattern.matcher(password).matches()
+
+    private fun isSignUpEmailValid(email: String) =
+        android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+            .matches() && email.length <= MAX_EMAIL_LENGTH
+
+    private fun isCorrectFormat(
+        fullName: String,
+        email: String,
+        password: String
+    ) = isSignUpFullNameValid(fullName) && isSignUpEmailValid(email) && isSignUpPasswordValid(
+        password
+    )
+
+    private fun handleRegisterFullNameTextChanged() {
+        edtUserName?.addTextChangedListener(onTextChanged = { p0: CharSequence?, _, _, _ ->
+            btnRegister?.isEnabled = isCorrectFormat(
+                p0.toString(),
+                edtEmail?.text.toString(),
+                edtPassword?.text.toString()
+            )
+            setBackgroundButton()
+        })
+    }
+
+    private fun handleRegisterEmailTextChanged() {
+        edtEmail?.addTextChangedListener(onTextChanged = { p0: CharSequence?, _, _, _ ->
+            btnRegister?.isEnabled = isCorrectFormat(
+                edtUserName?.text.toString(),
+                p0.toString(),
+                edtPassword?.text.toString()
+            )
+            setBackgroundButton()
+        })
+    }
+
+    private fun handleRegisterPasswordTextChanged() {
+        edtPassword?.addTextChangedListener(onTextChanged = { p0: CharSequence?, _, _, _ ->
+            btnRegister?.isEnabled = isCorrectFormat(
+                edtUserName?.text.toString(),
+                edtEmail?.text.toString(),
+                p0.toString()
+            )
+            setBackgroundButton()
+        })
+    }
+
+    private fun setBackgroundButton() {
+        if (btnRegister.isEnabled) {
+            btnRegister?.setBackgroundResource(R.drawable.bg_button_enabled)
+        } else {
+            btnRegister?.setBackgroundResource(R.drawable.bg_button_disabled)
+        }
+    }
+
+    private fun handleClickingArrowBack() {
+        imgArrowLeft?.setOnClickListener {
+            activity?.onBackPressed()
+        }
     }
 }
