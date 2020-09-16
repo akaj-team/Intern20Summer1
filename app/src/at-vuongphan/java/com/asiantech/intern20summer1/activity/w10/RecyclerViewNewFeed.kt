94cfd228c82510db2b8e0fd7c2acc1660c4d4c6d
at-vuongphan <vuong.phan@asiantech.vn>
2020-09-16 17:13:52 +0700

    fix comment CI

--- app/src/at-vuongphan/java/com/asiantech/intern20summer1/activity/w10/RecyclerViewNewFeed.kt
+++ app/src/at-vuongphan/java/com/asiantech/intern20summer1/activity/w10/RecyclerViewNewFeed.kt
@@ -1,20 +1,12 @@
 package com.asiantech.intern20summer1.activity.w10
 
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
-import android.view.View
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.asiantech.intern20summer1.R
-import com.asiantech.intern20summer1.adapter.w10.ItemFeedAdapter
 import com.asiantech.intern20summer1.fragment.w10.NewFeedFragment
-import com.asiantech.intern20summer1.model.w10.NewPost
 import com.asiantech.intern20summer1.model.w10.UserAutoSignIn
-import kotlinx.android.synthetic.`at-vuongphan`.w10_fragment_new_feed.*
 
 class RecyclerViewNewFeed : AppCompatActivity() {
     internal lateinit var token: String
