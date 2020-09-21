1593a9c34f7284c26580042ddd9120d2087cdd3e
at-linhle <linh.le@asiantech.vn>
2020-09-21 12:37:16 +0700

    fix comment

--- app/src/at-linhle/java/com/asiantech/intern20summer1/week12/viewmodels/HomeViewModel.kt
+++ app/src/at-linhle/java/com/asiantech/intern20summer1/week12/viewmodels/HomeViewModel.kt
@@ -1,7 +1,6 @@
 package com.asiantech.intern20summer1.week12.viewmodels
 
 import androidx.lifecycle.ViewModel
-import com.asiantech.intern20summer1.week12.api.ApiClient
 import com.asiantech.intern20summer1.week12.models.LikeResponse
 import com.asiantech.intern20summer1.week12.models.Post
 import com.asiantech.intern20summer1.week12.repository.RemoteRepository
@@ -9,9 +8,10 @@ import com.asiantech.intern20summer1.week12.repository.datasource.HomeDataSource
 import io.reactivex.Single
 import retrofit2.Response
 
-
 class HomeViewModel(private val repository: RemoteRepository) : ViewModel(), HomeDataSource {
-    override fun getListPost(token: String): Single<Response<MutableList<Post>>>? = repository.getListPost(token)
+    override fun getListPost(token: String): Single<Response<MutableList<Post>>>? =
+        repository.getListPost(token)
 
-    override fun updateLikePost(token: String, id: Int): Single<Response<LikeResponse>>? = repository.updateLikePost(token, id)
+    override fun updateLikePost(token: String, id: Int): Single<Response<LikeResponse>>? =
+        repository.updateLikePost(token, id)
 }
