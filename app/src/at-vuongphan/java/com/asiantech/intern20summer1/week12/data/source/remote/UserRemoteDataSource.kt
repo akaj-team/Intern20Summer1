243460a78a4e1e23c399a08709467f29e15bfa71
at-vuongphan <vuong.phan@asiantech.vn>
2020-09-29 09:35:17 +0700

    fix comment"

--- app/src/at-vuongphan/java/com/asiantech/intern20summer1/week12/data/source/remote/UserRemoteDataSource.kt
+++ app/src/at-vuongphan/java/com/asiantech/intern20summer1/week12/data/source/remote/UserRemoteDataSource.kt
@@ -4,11 +4,10 @@ import com.asiantech.intern20summer1.week12.data.source.remote.network.ClientAPI
 import com.asiantech.intern20summer1.week12.data.source.datasource.UserDataSource
 import com.asiantech.intern20summer1.week12.data.models.UserAutoSignIn
 import com.asiantech.intern20summer1.week12.data.models.UserRegister
-import com.asiantech.intern20summer1.week12.data.source.remote.network.UserClient
 import io.reactivex.Single
 import retrofit2.Response
 
-class UserRemoteDataSource() : UserDataSource {
+class UserRemoteDataSource : UserDataSource {
     val callRx = ClientAPI.createUserService()
     override fun login(username: String, password: String): Single<Response<UserAutoSignIn>>? =
         callRx?.login(username, password)
